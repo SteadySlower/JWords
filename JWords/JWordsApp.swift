0c67850f23ecaf669ede128ea606767ad3dc81db
JW Moon <moon910904@gmail.com>
2022-06-25 16:33:33 +0900

    Initial Commit

---
+++ JWords/JWordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JWordsApp.swift
+//  JWords
+//
+//  Created by JW Moon on 2022/06/25.
+//
+
+import SwiftUI
+
+@main
+struct JWordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
