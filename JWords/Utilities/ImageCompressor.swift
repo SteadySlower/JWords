4f4341b8ffbf52e0d3de1ce7f427bbc0563c3d3c
jwmoon <moon910904@gmail.com>
2022-07-01 15:15:29 +0900

    chore: ImageCompressor refactor

--- JWords/Utilities/ImageCompressor.swift
+++ JWords/Utilities/ImageCompressor.swift
@@ -14,7 +14,7 @@ class ImageCompressor {
         let jpegData = image.jpegData(compressionQuality: 0.5)!
         return jpegData
         #elseif os(macOS)
-        let cgImage = nsImage.cgImage(forProposedRect: nil, context: nil, hints: nil)!
+        let cgImage = image.cgImage(forProposedRect: nil, context: nil, hints: nil)!
         let bitmapRep = NSBitmapImageRep(cgImage: cgImage)
         let jpegData = bitmapRep.representation(using: NSBitmapImageRep.FileType.jpeg, properties: [:])!
         return jpegData
