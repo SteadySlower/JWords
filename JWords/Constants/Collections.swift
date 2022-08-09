54edc369323f906848af24601f0c91977eab72d0
jwmoon <moon910904@gmail.com>
2022-08-09 12:25:39 +0900

    chore: adjust collection constants

--- JWords/Constants/Collections.swift
+++ JWords/Constants/Collections.swift
@@ -9,7 +9,17 @@ import Firebase
 
 extension Constants {
     enum Collections {
-        static let wordBooks = Firestore.firestore().collection("books")
-        static func word(_ bookID: String) -> CollectionReference { Firestore.firestore().collection("books").document(bookID).collection("words") }
+        static let wordBooks = Firestore.firestore()
+            .collection("develop")
+            .document("data")
+            .collection("wordBooks")
+        static func word(_ bookID: String) -> CollectionReference {
+            Firestore.firestore()
+            .collection("develop")
+            .document("data")
+            .collection("wordBooks")
+            .document(bookID)
+            .collection("words")
+        }
     }
 }
