c581b25e710d2e86bde0910603d97f40c5e5b41c
JW Moon <moon910904@gmail.com>
2022-08-21 14:58:31 +0900

    feature: add wordbook in iOS

--- JWords/iOSView/Home/HomeView.swift
+++ JWords/iOSView/Home/HomeView.swift
@@ -10,18 +10,52 @@ import SwiftUI
 struct HomeView: View {
     
     @ObservedObject private var viewModel = ViewModel()
+    @State private var showModal = false
     
     var body: some View {
-        NavigationView {
-            ScrollView {
-                VStack(spacing: 8) {
-                    ForEach(viewModel.wordBooks) { wordBook in
-                        HomeCell(wordBook: wordBook)
-                    }
+        ScrollView {
+            VStack(spacing: 8) {
+                ForEach(viewModel.wordBooks) { wordBook in
+                    HomeCell(wordBook: wordBook)
                 }
             }
         }
+        .navigationTitle("단어장 목록")
         .onAppear { viewModel.fetchWordBooks() }
+        .sheet(isPresented: $showModal) { WordBookAddModal(viewModel: viewModel) }
+        .toolbar {
+            ToolbarItem {
+                Button("+") { showModal = true }
+            }
+        }
+    }
+}
+
+// MARK: SubViews
+extension HomeView {
+    private struct WordBookAddModal: View {
+        @State var WordBookTitle: String = ""
+        @Environment(\.presentationMode) var mode
+        private let viewModel: ViewModel
+        
+        init(viewModel: ViewModel) {
+            self.viewModel = viewModel
+        }
+        
+        var body: some View {
+            VStack {
+                TextField("단어장 이름", text: $WordBookTitle)
+                HStack {
+                    Button("추가", action: { viewModel.AddWordBook(WordBookTitle); dismiss()  })
+                    Button("취소", role: .cancel, action: { dismiss() })
+                }
+            }
+            .padding()
+        }
+        
+        private func dismiss() {
+            mode.wrappedValue.dismiss()
+        }
     }
 }
 
@@ -37,6 +71,16 @@ extension HomeView {
                 if let wordBooks = wordBooks {
                     self?.wordBooks = wordBooks
                 }
+            }
+        }
+        
+        func AddWordBook(_ title: String) {
+            WordService.saveBook(title: title) { [weak self] error in
+                if let error = error {
+                    print(error)
+                    return
+                }
+                self?.fetchWordBooks()
             }
         }
     }
