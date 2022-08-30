7315e796a7c58f7bcb84243039f072972976e0d9
jwmoon <moon910904@gmail.com>
2022-08-30 11:42:53 +0900

    feature: add function save examples from existing wordbooks in DataResettingScript

--- JWords/DataService/DataResettingScript.swift
+++ JWords/DataService/DataResettingScript.swift
@@ -34,29 +34,67 @@ class DataResettingScript {
 //        }
 //    }
     
-    // 각 단어장에서 추출한 단어들을 wordExamples로 만들어서 옮기는 과정
-    static func moveWordsToWordExamples() {
-        WordService.getWordBooks { books, error in
-            if let error = error {
-                print(error)
-                return
-            }
-            
-        }
-    }
+//    // 각 단어장에서 추출한 단어들을 wordExamples로 만들어서 옮기는 과정
+//    static func moveWordsToWordExamples() {
+//        // 단어장 전부 가져오기
+//        WordService.getWordBooks { books, error in
+//            if let error = error {
+//                print(error)
+//                return
+//            }
+//            guard let books = books else {
+//                print("books is nil")
+//                return
+//            }
+//            // 모든 단어장 순환해서
+//            for book in books {
+//                // 단어장에 있는 단어 전부 fetch 해오기
+//                WordService.getWords(wordBookID: book.id!) { words, error in
+//                    if let error = error {
+//                        print(error)
+//                        return
+//                    }
+//                    guard let words = words else {
+//                        print("words is nil in wordbooks \(book.id!)")
+//                        return
+//                    }
+//                    // 모든 단어 wordExample로 만들어서 저장
+//                    for word in words {
+//                        saveWordExample(word)
+//                    }
+//                }
+//            }
+//            
+//        }
+//    }
+//    
+//    // 이미 있는 단어를 examples에 저장하는 함수
+//    static func saveWordExample(_ word: Word) {
+//        let data: [String : Any] = ["timestamp": Timestamp(date: Date()),
+//                                    "meaningText": word.meaningText,
+//                                    "meaningImageURL": word.meaningImageURL,
+//                                    "ganaText": word.ganaText,
+//                                    "ganaImageURL": word.ganaImageURL,
+//                                    "kanjiText": word.kanjiText,
+//                                    "kanjiImageURL": word.kanjiImageURL,
+//                                    "used": 0]
+//        Constants.Collections.examples.addDocument(data: data)
+//    }
     
-    // 새로운 단어장을 모두 가져온다
-    private static func getNewWordBooks(wordBook: WordBook, completion: @escaping ([WordBook]) -> Void) {
-        Firestore.firestore()
-            .collection("develop")
-            .document("data")
-            .collection("wordBooks")
-            .getDocuments { snapshot, _ in
-                guard let documents = snapshot?.documents else { return }
-                let wordBooks = documents.compactMap({ try? $0.data(as: WordBook.self) })
-                completion(wordBooks)
-            }
-    }
+    // 각 단어장에서 단어들 모두 가져와서 WordExampleInput으로 바꾸는 함수
+    
+//    // 새로운 단어장을 모두 가져온다
+//    private static func getNewWordBooks(wordBook: WordBook, completion: @escaping ([WordBook]) -> Void) {
+//        Firestore.firestore()
+//            .collection("develop")
+//            .document("data")
+//            .collection("wordBooks")
+//            .getDocuments { snapshot, _ in
+//                guard let documents = snapshot?.documents else { return }
+//                let wordBooks = documents.compactMap({ try? $0.data(as: WordBook.self) })
+//                completion(wordBooks)
+//            }
+//    }
     
 //    // 기존 단어장의 단어장을 가져와서 새로운 단어장에 쓴다.
 //    private static func resetWordData(oldID: String, newID: String) {
