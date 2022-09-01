eb3239f4f8d57cf935b6058f338b628ef09790d8
jwmoon <moon910904@gmail.com>
2022-09-01 14:30:06 +0900

    feature: add WordDatabase Protocol

--- JWords/API/WordDatabase.swift
+++ JWords/API/WordDatabase.swift
@@ -8,18 +8,60 @@
 import Firebase
 
 protocol WordDatabase {
-    static func getWordBooks(completionHandler: @escaping ([WordBook]?, Error?) -> Void)
+    func fetchWordBooks(completionHandler: @escaping ([WordBook]?, Error?) -> Void)
+    func fetchWords(wordBookID id: String, completionHandler: @escaping ([Word]?, Error?) -> Void)
 }
 
-extension Firestore: WordDatabase {
-    static func getWordBooks(completionHandler: @escaping ([WordBook]?, Error?) -> Void) {
-        Constants.Collections.wordBooks.order(by: "timestamp", descending: true).getDocuments { snapshot, error in
+// Firebase에 직접 extension으로 만들어도 되지만 Firebase를 한단계 감싼 class를 만들었음.
+final class FirestoreWordDB: WordDatabase {
+    
+    // Firestore singleton
+    let firestore = Firestore.firestore()
+    
+    // CollectionReferences
+    private lazy var wordBookRef = {
+        firestore
+        .collection("develop")
+        .document("data")
+        .collection("wordBooks")
+    }()
+            
+    private func wordRef(of bookID: String) -> CollectionReference {
+        firestore
+        .collection("develop")
+        .document("data")
+        .collection("wordBooks")
+        .document(bookID)
+        .collection("words")
+    }
+    
+    private lazy var exampleRef = {
+        firestore
+        .collection("develop")
+        .document("data")
+        .collection("examples")
+    }()
+    
+    // API functions
+    func fetchWordBooks(completionHandler: @escaping ([WordBook]?, Error?) -> Void) {
+        wordBookRef.order(by: "timestamp", descending: true).getDocuments { snapshot, error in
             if let error = error {
                 completionHandler(nil, error)
             }
             guard let documents = snapshot?.documents else { return }
             let wordBooks = documents.compactMap({ try? $0.data(as: WordBook.self) })
             completionHandler(wordBooks, nil)
+        }
+    }
+    
+    func fetchWords(wordBookID id: String, completionHandler: @escaping ([Word]?, Error?) -> Void) {
+        wordRef(of: id).order(by: "timestamp").getDocuments { snapshot, error in
+            if let error = error {
+                completionHandler(nil, error)
+            }
+            guard let documents = snapshot?.documents else { return }
+            let words = documents.compactMap({ try? $0.data(as: Word.self) })
+            completionHandler(words, nil)
         }
     }
 }
