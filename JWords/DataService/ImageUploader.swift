0d665da8e168beaa23ea9e244055e4232a1f3a0a
jwmoon <moon910904@gmail.com>
2022-09-05 16:24:15 +0900

    feature: add protocol of ImageUploader

--- JWords/DataService/ImageUploader.swift
+++ JWords/DataService/ImageUploader.swift
@@ -8,15 +8,29 @@
 import Foundation
 import FirebaseStorage
 
-struct ImageUploader {
+protocol ImageUploader {
+    func uploadImage(image: InputImageType, group: DispatchGroup, completionHandler: @escaping (String) -> Void)
+}
+
+final class FirebaseIU: ImageUploader {
+    // Storage singleton
+    let store = Storage.storage()
+    
+    // Image Compressor
+    let ic: ImageCompressor
+    
+    init(imageCompressor: ImageCompressor) {
+        self.ic = imageCompressor
+    }
+    
     // SwiftUI의 Image는 아직 jpeg으로 압축할 수 없음. (UIImage 필요함)
-    static func uploadImage(image: InputImageType, group: DispatchGroup, completionHandler: @escaping(String) -> Void) {
+    func uploadImage(image: InputImageType, group: DispatchGroup, completionHandler: @escaping (String) -> Void) {
         group.enter()
         // 이미지 압축하기
-        let jpegData = ImageCompressor.compressImageToJPEG(image: image)
+        let jpegData = ic.compressImageToJPEG(image: image)
 
         // 이미지 경로 정하기
-        let ref = Storage.storage().reference(withPath: "/card_images/\(NSUUID().uuidString)")
+        let ref = store.reference(withPath: "/card_images/\(NSUUID().uuidString)")
         
         // 이미지 업로드
         ref.putData(jpegData, metadata: nil) { _, error in
