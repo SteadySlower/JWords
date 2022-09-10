8b0a32447f35c49835159fe3b27afc3840c9c4f8
JW Moon <moon910904@gmail.com>
2022-09-10 23:21:56 +0900

    feature: throw error at ImageCompressor when compression fails

--- JWords/Helper/Error.swift
+++ JWords/Helper/Error.swift
@@ -7,4 +7,14 @@
 
 enum AppError: Error {
     case generic(massage: String)
+    enum Utilities: Error {
+        case imageToDataFail
+        
+        var message: String {
+            switch self {
+            case .imageToDataFail:
+                return "Failed to compress image into data"
+            }
+        }
+    }
 }
--- JWords/Utilities/ImageCompressor.swift
+++ JWords/Utilities/ImageCompressor.swift
@@ -12,18 +12,24 @@ import Cocoa
 #endif
 
 protocol ImageCompressor {
-    func compressImageToJPEG(image: InputImageType) -> Data
+    func compressImageToJPEG(image: InputImageType) throws -> Data
 }
 
 class ImageCompressorImpl: ImageCompressor {
-    func compressImageToJPEG(image: InputImageType) -> Data {
+    func compressImageToJPEG(image: InputImageType) throws -> Data {
         #if os(iOS)
-        let jpegData = image.jpegData(compressionQuality: 0.5)!
+        guard let jpegData = image.jpegData(compressionQuality: 0.5) else {
+            throw AppError.Utilities.imageToDataFail
+            return
+        }
         return jpegData
         #elseif os(macOS)
         let cgImage = image.cgImage(forProposedRect: nil, context: nil, hints: nil)!
         let bitmapRep = NSBitmapImageRep(cgImage: cgImage)
-        let jpegData = bitmapRep.representation(using: NSBitmapImageRep.FileType.jpeg, properties: [:])!
+        guard let jpegData = bitmapRep.representation(using: NSBitmapImageRep.FileType.jpeg, properties: [:]) else {
+            throw AppError.Utilities.imageToDataFail
+            return
+        }
         return jpegData
         #endif
     }
