f6bfe2efbfaba0c4794dbd27c227440950542958
jwmoon <moon910904@gmail.com>
2022-09-15 15:23:47 +0900

    feature: add initializer error in Word Model

--- JWords/Model/Word.swift
+++ JWords/Model/Word.swift
@@ -21,7 +21,8 @@ protocol Word {
     var kanjiText: String { get }
     var kanjiImageURL: String { get }
     var studyState: StudyState { get set }
-    var timestamp: Date { get }
+    var createdAt: Date { get }
+    
     var hasImage: Bool { get }
 }
 
@@ -36,25 +37,37 @@ struct WordImpl: Word {
     let kanjiText: String
     let kanjiImageURL: String
     var studyState: StudyState
-    let timestamp: Date
+    let createdAt: Date
     
     var hasImage: Bool {
         !self.meaningImageURL.isEmpty || !self.ganaImageURL.isEmpty || !self.kanjiImageURL.isEmpty
     }
     
-    // TODO: Handle Parsing Error
-    init(id: String, wordBookID: String, dict: [String : Any]) {
+    init(id: String, wordBookID: String, dict: [String : Any]) throws {
         self.id = id
         self.wordBookID = wordBookID
-        self.meaningText = dict["meaningText"] as? String ?? ""
-        self.meaningImageURL = dict["meaningImageURL"] as? String ?? ""
-        self.ganaText = dict["ganaText"] as? String ?? ""
-        self.ganaImageURL = dict["ganaImageURL"] as? String ?? ""
-        self.kanjiText = dict["kanjiText"] as? String ?? ""
-        self.kanjiImageURL = dict["kanjiImageURL"] as? String ?? ""
-        let rawValue = dict["studyState"] as? Int ?? 0
-        self.studyState = StudyState(rawValue: rawValue) ?? .undefined
-        self.timestamp = dict["timestamp"] as? Date ?? Date()
+        
+        if let meaningText = dict["meaningText"] as? String,
+           let meaningImageURL = dict["meaningImageURL"] as? String,
+           let ganaText = dict["ganaText"] as? String,
+           let ganaImageURL = dict["ganaImageURL"] as? String,
+           let kanjiText = dict["kanjiText"] as? String,
+           let kanjiImageURL = dict["kanjiImageURL"] as? String,
+           let rawValue = dict["studyState"] as? Int,
+           let studyState = StudyState(rawValue: rawValue),
+           let createdAt = dict["timestamp"] as? Date
+        {
+            self.meaningText = meaningText
+            self.meaningImageURL = meaningImageURL
+            self.ganaText = ganaText
+            self.ganaImageURL = ganaImageURL
+            self.kanjiText = kanjiText
+            self.kanjiImageURL = kanjiImageURL
+            self.studyState = studyState
+            self.createdAt = createdAt
+        } else {
+            throw AppError.generic(massage: "init")
+        }
     }
 }
 
