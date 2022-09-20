4095cc2d51ab6159c19b264b31b13a6edf84082a
jwmoon <moon910904@gmail.com>
2022-09-20 15:23:20 +0900

    feature: make not to close wordBooks when failed words are moved to other wordbook

--- JWords/DataService/WordBookService.swift
+++ JWords/DataService/WordBookService.swift
@@ -37,12 +37,14 @@ class WordBookServiceImpl: WordBookService {
     // TODO: 복습으로 체크하는 기능 생기면 복습으로 체크하는 함수로 바꾸고 닫기 함수는 따로 만들기
     func moveWords(of toClose: WordBook, to destination: WordBook?, toMove: [Word], completionHandler: @escaping CompletionWithoutData) {
         if let destination = destination {
-            wordService.copyWords(toMove, to: destination) { [weak self] error in
-                if let error = error { completionHandler(error) }
-                self?.db.closeWordBook(of: toClose, completionHandler: completionHandler)
+            wordService.moveWords(toMove, to: destination) { error in
+                completionHandler(error)
+//                if let error = error { completionHandler(error) }
+//                self?.db.closeWordBook(of: toClose, completionHandler: completionHandler)
             }
         } else {
-            db.closeWordBook(of: toClose, completionHandler: completionHandler)
+            completionHandler(nil)
+//            db.closeWordBook(of: toClose, completionHandler: completionHandler)
         }
     }
     
