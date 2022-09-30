030e536de339fa2179778b1211871296b5691e62
jwmoon <moon910904@gmail.com>
2022-09-30 12:13:35 +0900

    feature: add tests for words

--- JWordsUnitTests/StudyViewModelTest.swift
+++ JWordsUnitTests/StudyViewModelTest.swift
@@ -25,6 +25,15 @@ class StudyViewModelTest: QuickSpec {
             viewModel = StudyView.ViewModel(words: words, wordService: wordService)
         }
         
+        func randomWords(_ count: Int) -> [Word] {
+            var words = [Word]()
+            for _ in 0..<count {
+                words.append(MockWord())
+            }
+            return words
+        }
+
+        
         describe("wordBook") {
             context("when viewModel is initialized with a wordBook") {
                 beforeEach {
@@ -36,14 +45,46 @@ class StudyViewModelTest: QuickSpec {
             }
             context("when viewModel is initialized with words") {
                 beforeEach {
-                    var words = [Word]()
-                    for _ in 0..<Random.int(from: 1, to: 100) {
-                        words.append(MockWord())
-                    }
-                    prepare(words: words)
+                    prepare(words: randomWords(Random.int(from: 1, to: 100)))
                 }
                 it("should be nil") {
                     expect(viewModel.wordBook).to(beNil())
+                }
+            }
+        }
+        
+        describe("words") {
+            context("when viewModel is initialized with a wordBook") {
+                beforeEach {
+                    prepare()
+                }
+                context("when words are fetched successfully") {
+                    let count = Random.int(from: 1, to: 100)
+                    beforeEach {
+                        wordService.getWordsSuccess = randomWords(count)
+                        viewModel.fetchWords()
+                    }
+                    it("should have the same count with getWordsSuccess") {
+                        expect(viewModel.words.count).to(equal(count))
+                    }
+                }
+                context("when words fails to be fetched") {
+                    beforeEach {
+                        viewModel.fetchWords()
+                    }
+                    it("should be empty") {
+                        expect(viewModel.words).to(beEmpty())
+                    }
+                }
+            }
+            context("when viewModel is initialized with words") {
+                let count = Random.int(from: 1, to: 100)
+                beforeEach {
+                    let words = randomWords(count)
+                    prepare(words: words)
+                }
+                it("should have the same count with the words") {
+                    expect(viewModel.words.count).to(equal(count))
                 }
             }
         }
