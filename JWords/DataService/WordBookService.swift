b057f0ef6a014fa56b09ef8b85f98fa1e9a366ea
jwmoon <moon910904@gmail.com>
2022-10-28 11:21:30 +0900

    chore: remove remarks

--- JWords/DataService/WordBookService.swift
+++ JWords/DataService/WordBookService.swift
@@ -35,17 +35,13 @@ class WordBookServiceImpl: WordBookService {
         db.checkIfOverlap(wordBook: wordBook, meaningText: meaningText, completionHandler: completionHandler)
     }
     
-    // TODO: 복습으로 체크하는 기능 생기면 복습으로 체크하는 함수로 바꾸고 닫기 함수는 따로 만들기
     func moveWords(of toClose: WordBook, to destination: WordBook?, toMove: [Word], completionHandler: @escaping CompletionWithoutData) {
         if let destination = destination {
             wordService.moveWords(toMove, to: destination) { error in
                 completionHandler(error)
-//                if let error = error { completionHandler(error) }
-//                self?.db.closeWordBook(of: toClose, completionHandler: completionHandler)
             }
         } else {
             completionHandler(nil)
-//            db.closeWordBook(of: toClose, completionHandler: completionHandler)
         }
     }
     
