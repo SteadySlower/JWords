fc2ab09e522d747f3a915eb768404e36c52be8ec
JW Moon <moon910904@gmail.com>
2023-03-05 13:30:26 +0900

    feature: add rStrip and lStrip method in String type

---
+++ JWords/Helper/String+Extension.swift
@@ -0,0 +1,38 @@
+//
+//  String+Extension.swift
+//  JWords
+//
+//  Created by JW Moon on 2023/03/04.
+//
+
+import Foundation
+
+extension String {
+    
+    static let whitespaceAndNewlineCharacters: [Character] = [" ", "\n", "\t"]
+
+    func rStrip(_ charactors: [Character] = String.whitespaceAndNewlineCharacters) -> String {
+        var s = self
+        guard s.contains(where: { charactors.contains($0) }) else { return self }
+        while let last = s.last,
+              charactors.contains(last) {
+            s = String(s.dropLast())
+            print("rstrip")
+        }
+        print(s)
+        return s
+        
+    }
+    
+    func lStrip(_ charactors: [Character] = String.whitespaceAndNewlineCharacters) -> String {
+        var s = self
+        guard s.contains(where: { charactors.contains($0) }) else { return self }
+        while let first = s.first,
+              charactors.contains(first) {
+            s = String(s.dropFirst())
+            print("lstrip")
+        }
+        print(s)
+        return s
+    }
+}
