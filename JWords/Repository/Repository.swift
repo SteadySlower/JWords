3fe8025ec90d875f4c26ec67296be3deb5163a92
JW Moon <moon910904@gmail.com>
2023-03-18 13:34:57 +0900

    feature: add base repository

---
+++ JWords/Repository/Repository.swift
@@ -0,0 +1,46 @@
+//
+//  Repository.swift
+//  JWords
+//
+//  Created by JW Moon on 2023/03/18.
+//
+
+import Foundation
+import Combine
+
+protocol BaseRepository {
+    var isLoading: AnyPublisher<Bool, Never> { get }
+    var error: AnyPublisher<Error?, Never> { get }
+    var subscription: [AnyCancellable] { get }
+    func clear() -> Void
+}
+
+class Repository {
+    
+    private let _isLoading = CurrentValueSubject<Bool, Never>(false)
+    
+    private let _error = PassthroughSubject<Error, Never>()
+    
+    var isLoading: AnyPublisher<Bool, Never> {
+        _isLoading.eraseToAnyPublisher()
+    }
+    
+    var error: AnyPublisher<Error, Never> {
+        _error.eraseToAnyPublisher()
+    }
+    
+    var subscription: [AnyCancellable] = []
+    
+    final private func isLoading(_ isLoading: Bool) {
+        self._isLoading.send(isLoading)
+    }
+    
+    final private func onError(_ error: Error) {
+        self._error.send(error)
+    }
+    
+    func clear() {
+        subscription = []
+    }
+    
+}
