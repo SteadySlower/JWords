604801d098bcfdc6d234ca61dfac938816535b60
jwmoon <moon910904@gmail.com>
2023-04-28 15:46:08 +0900

    chore: add image logic in GanaField

--- JWords/macView/Components/GanaField.swift
+++ JWords/macView/Components/GanaField.swift
@@ -13,11 +13,16 @@ struct AddGana: ReducerProtocol {
     
     struct State: Equatable {
         var text = ""
+        var image: InputImageType?
         var autoConvert: Bool = true
     }
     
+    @Dependency(\.pasteBoardClient) var pasteBoardClient
+    
     enum Action: Equatable {
         case updateText(String)
+        case imageAddButtonTapped
+        case imageTapped
         case updateAutoConvert(Bool)
         case onTab
     }
@@ -28,6 +33,12 @@ struct AddGana: ReducerProtocol {
             case .updateText(let text):
                 if text.hasTab { return .task { .onTab } }
                 state.text = text
+                return .none
+            case .imageAddButtonTapped:
+                state.image = pasteBoardClient.fetchImage()
+                return .none
+            case .imageTapped:
+                state.image = nil
                 return .none
             case .updateAutoConvert(let bool):
                 state.autoConvert = bool
@@ -61,7 +72,23 @@ struct GanaField: View {
                        isOn: vs.binding(
                         get: \.autoConvert,
                         send: AddGana.Action.updateAutoConvert))
-                
+                if let image = vs.image {
+                    Group {
+                        #if os(iOS)
+                        Image(uiImage: image).resizable()
+                        #elseif os(macOS)
+                        Image(nsImage: image).resizable()
+                        #endif
+                    }
+                    .frame(width: Constants.Size.deviceWidth * 0.8, height: 150)
+                    .onTapGesture { vs.send(.imageTapped) }
+                } else {
+                    Button {
+                        vs.send(.imageAddButtonTapped)
+                    } label: {
+                        Text("뜻 이미지")
+                    }
+                }
             }
 
         }
