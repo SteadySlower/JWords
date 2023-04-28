0ad6b10e96a5b20d15dfde611943d9531da3a88c
jwmoon <moon910904@gmail.com>
2023-04-28 15:47:58 +0900

    chore: add image logic in kanji field

--- JWords/macView/Components/KanjiField.swift
+++ JWords/macView/Components/KanjiField.swift
@@ -13,10 +13,15 @@ struct AddKanji: ReducerProtocol {
     
     struct State: Equatable {
         var text = ""
+        var image: InputImageType?
     }
+    
+    @Dependency(\.pasteBoardClient) var pasteBoardClient
     
     enum Action: Equatable {
         case updateText(String)
+        case imageAddButtonTapped
+        case imageTapped
         case onTab
     }
     
@@ -26,6 +31,12 @@ struct AddKanji: ReducerProtocol {
             case .updateText(let text):
                 if text.hasTab { return .task { .onTab } }
                 state.text = text
+                return .none
+            case .imageAddButtonTapped:
+                state.image = pasteBoardClient.fetchImage()
+                return .none
+            case .imageTapped:
+                state.image = nil
                 return .none
             case .onTab:
                 print("디버그: tab on gana field")
@@ -52,6 +63,23 @@ struct KanjiField: View {
                     .font(.system(size: 30))
                     .frame(height: Constants.Size.deviceHeight / 8)
                     .padding(.horizontal)
+                if let image = vs.image {
+                    Group {
+                        #if os(iOS)
+                        Image(uiImage: image).resizable()
+                        #elseif os(macOS)
+                        Image(nsImage: image).resizable()
+                        #endif
+                    }
+                    .frame(width: Constants.Size.deviceWidth * 0.8, height: 150)
+                    .onTapGesture { vs.send(.imageTapped) }
+                } else {
+                    Button {
+                        vs.send(.imageAddButtonTapped)
+                    } label: {
+                        Text("뜻 이미지")
+                    }
+                }
             }
 
         }
