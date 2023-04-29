2a8f1f63b2b9cd12d89557cafb86245c2d916476
JW Moon <moon910904@gmail.com>
2023-04-29 16:20:20 +0900

    feature: add HuriganaConverter and isKanji extension

---
+++ JWords/Utilities/HuriganaConverter.swift
@@ -0,0 +1,16 @@
+//
+//  HuriganaConverter.swift
+//  JWords
+//
+//  Created by JW Moon on 2023/04/29.
+//
+
+import Foundation
+
+class HuriganaConverter {
+    
+    static let shared = HuriganaConverter()
+    
+    
+    
+}
