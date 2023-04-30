0bc9ad47d46e6a3dfaa47552ec3e54f0053e02fa
JW Moon <moon910904@gmail.com>
2023-04-30 14:06:03 +0900

    chore: add WrappingHStack file

---
+++ JWords/iOSView/items/EditableHuriganaText.swift
@@ -0,0 +1,20 @@
+//
+//  EditableHuriganaText.swift
+//  JWords
+//
+//  Created by JW Moon on 2023/04/30.
+//
+
+import SwiftUI
+
+struct EditableHuriganaText: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditableHuriganaText_Previews: PreviewProvider {
+    static var previews: some View {
+        EditableHuriganaText()
+    }
+}
