40c1549c483ac30364b525350009d0222308374b
JW Moon <moon910904@gmail.com>
2023-04-30 12:52:33 +0900

    chore: refactor HuriganaText with Layout

--- JWords/macView/Components/HuriganaText.swift
+++ JWords/macView/Components/HuriganaText.swift
@@ -25,6 +25,59 @@ private struct Huri: Identifiable, Equatable {
     }
 }
 
+private struct WrappingHStack: Layout {
+    private var horizontalSpacing: CGFloat
+    private var verticalSpacing: CGFloat
+    public init(horizontalSpacing: CGFloat, verticalSpacing: CGFloat? = nil) {
+        self.horizontalSpacing = horizontalSpacing
+        self.verticalSpacing = verticalSpacing ?? horizontalSpacing
+    }
+
+    public func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache _: inout ()) -> CGSize {
+        guard !subviews.isEmpty else { return .zero }
+
+        let height = subviews.map { $0.sizeThatFits(proposal).height }.max() ?? 0
+
+        var rowWidths = [CGFloat]()
+        var currentRowWidth: CGFloat = 0
+        subviews.forEach { subview in
+            if currentRowWidth + horizontalSpacing + subview.sizeThatFits(proposal).width >= proposal.width ?? 0 {
+                rowWidths.append(currentRowWidth)
+                currentRowWidth = subview.sizeThatFits(proposal).width
+            } else {
+                currentRowWidth += horizontalSpacing + subview.sizeThatFits(proposal).width
+            }
+        }
+        rowWidths.append(currentRowWidth)
+
+        let rowCount = CGFloat(rowWidths.count)
+        return CGSize(width: max(rowWidths.max() ?? 0, proposal.width ?? 0), height: rowCount * height + (rowCount - 1) * verticalSpacing)
+    }
+
+    public func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) {
+        let height = subviews.map { $0.dimensions(in: proposal).height }.max() ?? 0
+        guard !subviews.isEmpty else { return }
+        var x = bounds.minX
+        var y = height / 2 + bounds.minY
+        subviews.forEach { subview in
+            x += subview.dimensions(in: proposal).width / 2
+            if x + subview.dimensions(in: proposal).width / 2 > bounds.maxX {
+                x = bounds.minX + subview.dimensions(in: proposal).width / 2
+                y += height + verticalSpacing
+            }
+            subview.place(
+                at: CGPoint(x: x, y: y),
+                anchor: .center,
+                proposal: ProposedViewSize(
+                    width: subview.dimensions(in: proposal).width,
+                    height: subview.dimensions(in: proposal).height
+                )
+            )
+            x += subview.dimensions(in: proposal).width / 2 + horizontalSpacing
+        }
+    }
+}
+
 struct HuriganaText: View {
     private let huris: [Huri]
     private let fontSize: CGFloat
@@ -35,46 +88,15 @@ struct HuriganaText: View {
     }
 
     var body: some View {
-        GeometryReader { geometry in
-            self.generateContent(in: geometry)
-        }
-    }
-
-    private func generateContent(in g: GeometryProxy) -> some View {
-        var width = CGFloat.zero
-        var height = CGFloat.zero
-
-        return ZStack(alignment: .topLeading) {
-            ForEach(huris, id: \.id) { huri in
-                self.item(for: huri)
-                    .padding(.vertical, 4)
-                    .alignmentGuide(.leading, computeValue: { d in
-                        if (abs(width - d.width) > g.size.width)
-                        {
-                            width = 0
-                            height -= d.height
-                        }
-                        let result = width
-                        if huri == self.huris.last! {
-                            width = 0 //last item
-                        } else {
-                            width -= d.width
-                        }
-                        return result
-                    })
-                    .alignmentGuide(.top, computeValue: {d in
-                        let result = height
-                        if huri == self.huris.last! {
-                            height = 0 // last item
-                        }
-                        return result
-                    })
+        WrappingHStack(horizontalSpacing: 0, verticalSpacing: fontSize / 2) {
+            ForEach(huris) { huri in
+                huriView(for: huri)
             }
         }
     }
-
+    
     @ViewBuilder
-    private func item(for huri: Huri) -> some View {
+    private func huriView(for huri: Huri) -> some View {
         if !huri.kanji.isEmpty {
             ZStack {
                 Text(huri.kanji)
