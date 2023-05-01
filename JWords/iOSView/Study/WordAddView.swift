db74713a863b84be79615398af508179f417eb74
JW Moon <moon910904@gmail.com>
2023-05-01 20:42:42 +0900

    feature: make WordAddView Prototype

--- JWords/iOSView/Study/WordAddView.swift
+++ JWords/iOSView/Study/WordAddView.swift
@@ -12,18 +12,34 @@ struct WordAdding: ReducerProtocol {
     struct State: Equatable {
         var meaningText: String = ""
         var wordText: String = ""
-        var huriText: String = ""
+        var huriText = EditHuriganaText.State(hurigana: "")
+        
+        mutating func updateWordText(_ text: String) {
+            wordText = text
+            let hurigana = HuriganaConverter.shared.convert(text)
+            huriText = EditHuriganaText.State(hurigana: hurigana)
+        }
     }
     
     enum Action: Equatable {
+        case updateWordText(String)
+        case editHuriText(action: EditHuriganaText.Action)
+        case addButtonTapped
+        case cancelButtonTapped
     }
     
     var body: some ReducerProtocol<State, Action> {
         Reduce { state, action in
             switch action {
+            case .updateWordText(let text):
+                state.updateWordText(text)
+                return .none
             default:
                 return .none
             }
+        }
+        Scope(state: \.huriText, action: /Action.editHuriText(action:)) {
+            EditHuriganaText()
         }
     }
 
@@ -36,7 +52,17 @@ struct WordAddView: View {
     var body: some View {
         WithViewStore(store, observe: { $0 }) { vs in
             VStack {
-                
+                EditableHuriganaText(store: store.scope(
+                    state: \.huriText,
+                    action: WordAdding.Action.editHuriText(action:))
+                )
+                TextEditor(text: vs.binding(get: \.wordText, send: WordAdding.Action.updateWordText))
+                    .border(.black)
+                    .frame(height: 100)
+                HStack {
+                    Button("추가") { vs.send(.addButtonTapped) }
+                    Button("취소") { vs.send(.cancelButtonTapped) }
+                }
             }
         }
     }
