8b940310cb2c095284a7c626b0a84f9f1adf90b6
jwmoon <moon910904@gmail.com>
2023-05-15 10:01:13 +0900

    feature: add kanji list view

---
+++ JWords/iOSView/Kanji/KanjiList.swift
@@ -0,0 +1,20 @@
+//
+//  KanjiList.swift
+//  JWords
+//
+//  Created by Jong Won Moon on 2023/05/15.
+//
+
+import SwiftUI
+
+struct KanjiList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct KanjiList_Previews: PreviewProvider {
+    static var previews: some View {
+        KanjiList()
+    }
+}
