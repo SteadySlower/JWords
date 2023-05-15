ab23b1c39bbc22ffc108cafa26eb8e9e88d1a450
jwmoon <moon910904@gmail.com>
2023-05-15 11:16:13 +0900

    feature: design in kanji cell

--- JWords/iOSView/Kanji/KanjiCell.swift
+++ JWords/iOSView/Kanji/KanjiCell.swift
@@ -12,12 +12,25 @@ struct KanjiCell: View {
     let kanji: Kanji
     
     var body: some View {
-        HStack {
-            VStack(spacing: 10) {
-                Text(kanji.kanjiText ?? "")
-                    .font(.system(size: 40))
-                Text(kanji.meaningText ?? "❓")
-                    .font(.system(size: 20))
+        ZStack {
+            HStack {
+                VStack(spacing: 10) {
+                    Text(kanji.kanjiText ?? "")
+                        .font(.system(size: 40))
+                    Text(kanji.meaningText ?? "❓")
+                        .font(.system(size: 20))
+                }
+            }
+            HStack {
+                Spacer()
+                VStack(spacing: 10) {
+                    Button("✏️") {
+                        
+                    }
+                    Button("단어보기") {
+                        
+                    }
+                }
             }
         }
         .frame(width: Constants.Size.deviceWidth * 0.9)
