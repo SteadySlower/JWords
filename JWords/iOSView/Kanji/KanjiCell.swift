217a69144c9f4491ee26724fa8e69e17fb08f97c
jwmoon <moon910904@gmail.com>
2023-05-16 14:29:30 +0900

    chore: refactor KanjiCell with TCA

--- JWords/iOSView/Kanji/KanjiCell.swift
+++ JWords/iOSView/Kanji/KanjiCell.swift
@@ -6,65 +6,104 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
+
+struct StudyKanji: ReducerProtocol {
+    struct State: Equatable, Identifiable {
+        let id: String
+        let kanji: Kanji
+        var meaningText: String = ""
+        var isEditing: Bool = false
+        
+        var displayMeaning: String {
+            if let meaning = kanji.meaningText {
+                return !meaning.isEmpty ? meaning : "???"
+            } else {
+                return "???"
+            }
+        }
+        
+        init(kanji: Kanji) {
+            self.id = kanji.id
+            self.kanji = kanji
+        }
+    }
+    
+    enum Action: Equatable {
+        case editButtonTapped
+        case updateMeaningText(String)
+        case wordButtonTapped
+        case inputButtonTapped
+        case onKanjiEdited(Kanji)
+    }
+    
+    private let cd = CoreDataClient.shared
+    
+    var body: some ReducerProtocol<State, Action> {
+        Reduce { state, action in
+            switch action {
+            case .editButtonTapped:
+                state.isEditing = true
+                state.meaningText = state.kanji.meaningText ?? ""
+                return .none
+            case .updateMeaningText(let text):
+                state.meaningText = text
+                return .none
+            case .inputButtonTapped:
+                guard !state.meaningText.isEmpty else { return .none }
+                let edited = try! cd.editKanji(kanji: state.kanji, meaningText: state.meaningText)
+                return .task { .onKanjiEdited(edited) }
+            default:
+                return .none
+            }
+        }
+    }
+
+}
 
 struct KanjiCell: View {
     
-    private let kanji: Kanji
-    private let editKanjiMeaning: (Kanji, String) -> Void
-    private let wordButtonTapped: () -> Void
-    
-    @State var meaningText: String = ""
-    @State var isEditing: Bool = false
-    
-    init(kanji: Kanji, editKanjiMeaning: @escaping (Kanji, String) -> Void, wordButtonTapped: @escaping () -> Void) {
-        self.kanji = kanji
-        self.editKanjiMeaning = editKanjiMeaning
-        self.wordButtonTapped = wordButtonTapped
-    }
+    let store: StoreOf<StudyKanji>
     
     var body: some View {
-        ZStack {
-            HStack {
-                VStack(spacing: 10) {
-                    Text(kanji.kanjiText ?? "")
-                        .font(.system(size: 40))
-                    if isEditing {
-                        meaningField
-                    } else {
-                        Text(kanji.meaningText ?? "❓")
-                            .font(.system(size: 20))
+        WithViewStore(store, observe: { $0 }) { vs in
+            ZStack {
+                HStack {
+                    VStack(spacing: 10) {
+                        Text(vs.kanji.kanjiText ?? "")
+                            .font(.system(size: 40))
+                        if vs.isEditing {
+                            HStack {
+                                TextField("뜻 입력", text: vs.binding(get: \.meaningText, send: StudyKanji.Action.updateMeaningText))
+                                    .frame(width: Constants.Size.deviceWidth * 0.3)
+                                    .border(.black)
+                                    .multilineTextAlignment(.center)
+                                Button("입력") { vs.send(.inputButtonTapped) }
+                            }
+                        } else {
+                            Text(vs.kanji.meaningText ?? "???")
+                                .font(.system(size: 20))
+                        }
+                    }
+                }
+                HStack {
+                    Spacer()
+                    VStack(spacing: 10) {
+                        Button("✏️") {
+                            vs.send(.editButtonTapped)
+                        }
+                        Button("단어 보기") {
+                            vs.send(.wordButtonTapped)
+                        }
                     }
                 }
             }
-            HStack {
-                Spacer()
-                VStack(spacing: 10) {
-                    Button("✏️") {
-                        meaningText = kanji.meaningText ?? ""
-                        isEditing = true
-                    }
-                    Button("단어 보기") {
-                        wordButtonTapped()
-                    }
-                }
-            }
-        }
-        .frame(width: Constants.Size.deviceWidth * 0.9)
-        .padding(.vertical, 5)
-        .border(.black)
-        .padding(.vertical, 5)
-    }
-    
-    private var meaningField: some View {
-        HStack {
-            TextField("뜻 입력", text: $meaningText)
-                .frame(width: Constants.Size.deviceWidth * 0.3)
-                .border(.black)
-            Button("입력") {
-                isEditing = false
-                editKanjiMeaning(kanji, meaningText)
-            }
+            .frame(width: Constants.Size.deviceWidth * 0.9)
+            .padding(.vertical, 5)
+            .border(.black)
+            .padding(.vertical, 5)
         }
     }
+
     
 }
