6a7b518460ba1944dad5d49e1734758c34a0fab2
JW Moon <moon910904@gmail.com>
2023-05-21 08:25:38 +0900

    feature: add set picker

--- JWords/macView/MacStudyView.swift
+++ JWords/macView/MacStudyView.swift
@@ -10,16 +10,24 @@ import ComposableArchitecture
 
 struct MacWordList: ReducerProtocol {
     struct State: Equatable {
+        var setList = [StudySet]()
+        var selectedID: String? = nil
         
+        var selectedSet: StudySet? {
+            setList.first(where: { $0.id == selectedID })
+        }
     }
     
     enum Action: Equatable {
-        
+        case updateNowSet(String?)
     }
     
     var body: some ReducerProtocol<State, Action> {
         Reduce { state, action in
             switch action {
+            case .updateNowSet(let id):
+                state.selectedID = id
+                return .none
             default:
                 return .none
             }
@@ -34,7 +42,20 @@ struct MacStudyView: View {
     
     var body: some View {
         WithViewStore(store, observe: { $0 }) { vs in
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            VStack {
+                Picker("", selection:
+                        vs.binding(
+                             get: \.selectedID,
+                             send: MacWordList.Action.updateNowSet)
+                ) {
+                    Text("선택된 단어장 없음")
+                        .tag(nil as String?)
+                    ForEach(vs.setList, id: \.id) { book in
+                        Text(book.title)
+                            .tag(book.id as String?)
+                    }
+                }
+            }
         }
     }
 }
