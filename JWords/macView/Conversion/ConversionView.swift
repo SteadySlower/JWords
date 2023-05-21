2261e39e4bc52a3813cb71e23f0bdd24dd8ff222
JW Moon <moon910904@gmail.com>
2023-05-21 14:33:55 +0900

    feature: add firebase cells in conversion view

--- JWords/macView/Conversion/ConversionView.swift
+++ JWords/macView/Conversion/ConversionView.swift
@@ -12,24 +12,45 @@ struct ConversionList: ReducerProtocol {
     struct State: Equatable {
         var coredataSet = SelectStudySet.State(pickerName: "CoreData 단어장")
         var firebaseBook = SelectWordBook.State(pickerName: "Firebase 단어장")
+        var words: IdentifiedArrayOf<FirebaseWord.State> = []
     }
     
     enum Action: Equatable {
         case selectStudySet(action: SelectStudySet.Action)
         case selectWordBook(action: SelectWordBook.Action)
+        case wordsResponse(TaskResult<[Word]>)
+        case word(id: FirebaseWord.State.ID, action: FirebaseWord.Action)
     }
     
     private let cd = CoreDataClient.shared
     @Dependency(\.wordBookClient) var wordBookClient
+    @Dependency(\.wordClient) var wordClient
     private enum fetchBooksID {}
     
     var body: some ReducerProtocol<State, Action> {
         Reduce { state, action in
             switch action {
+            case .selectWordBook(let action):
+                switch action {
+                case .updateID(_):
+                    guard let book = state.firebaseBook.selectedBook else { return .none }
+                    return .task {
+                        await .wordsResponse( TaskResult { try await wordClient.words(book) } )
+                    }
+                default: break
+                }
+            case let .wordsResponse(.success(words)):
+                state.words = IdentifiedArrayOf(
+                    uniqueElements: words.map {
+                        FirebaseWord.State(word: $0)
+                    })
             default:
                 break
             }
             return .none
+        }
+        .forEach(\.words, action: /Action.word(id:action:)) {
+            FirebaseWord()
         }
         Scope(state: \.coredataSet, action: /Action.selectStudySet(action:)) {
             SelectStudySet()
@@ -53,17 +74,20 @@ struct ConversionView: View {
                         state: \.coredataSet,
                         action: ConversionList.Action.selectStudySet(action:))
                     )
-                    WordBookPicker(store: store.scope(
-                        state: \.firebaseBook,
-                        action: ConversionList.Action.selectWordBook(action:))
-                    )
-                }
-                HStack {
-                    ScrollView {
-                        
-                    }
-                    ScrollView {
-                        
+                    VStack {
+                        WordBookPicker(store: store.scope(
+                            state: \.firebaseBook,
+                            action: ConversionList.Action.selectWordBook(action:))
+                        )
+                        ScrollView {
+                            VStack(spacing: 10) {
+                                ForEachStore(
+                                    self.store.scope(state: \.words, action: ConversionList.Action.word(id:action:))
+                                  ) {
+                                      FirebaseWordCell(store: $0)
+                                  }
+                            }
+                        }
                     }
                 }
             }
