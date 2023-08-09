d5fd46b666a839f12170f37596eca89dc14b3ff2
jwmoon <moon910904@gmail.com>
2023-08-09 10:48:21 +0900

    feature: add public methods in KVStorageClient

--- JWords/API/KVStorageClient.swift
+++ JWords/API/KVStorageClient.swift
@@ -41,5 +41,33 @@ final class KVStorageClient {
 // public Methods
 
 extension KVStorageClient {
+    func fetchSchedule() -> TodaySchedule {
+        TodaySchedule(studyIDs: arrayOfString(for: .studySets),
+                      reviewIDs: arrayOfString(for: .reviewSets),
+                      reviewedIDs: arrayOfString(for: .reviewedSets),
+                      createdAt: date(for: .createdAt))
+    }
     
+    func autoSetSchedule(sets: [StudySet]) {
+        let studySets = sets.filter { $0.schedule == .study }.map { $0.id }
+        let reviewSets = sets.filter { $0.schedule == .review }.map { $0.id }
+        setArrayOfString(key: .studySets, value: studySets)
+        setArrayOfString(key: .reviewSets, value: reviewSets)
+        setArrayOfString(key: .reviewedSets, value: [])
+        setDate(key: .createdAt, value: Date())
+    }
+    
+    func updateSchedule(todaySchedule: TodaySchedule) {
+        setArrayOfString(key: .studySets, value: todaySchedule.studyIDs)
+        setArrayOfString(key: .reviewSets, value: todaySchedule.reviewIDs)
+        setArrayOfString(key: .reviewedSets, value: todaySchedule.reviewedIDs)
+        setDate(key: .createdAt, value: Date())
+    }
+    
+    func addReviewedSet(reviewed: StudySet) {
+        var reviewedIDs = arrayOfString(for: .reviewedSets)
+        reviewedIDs.append(reviewed.id)
+        setArrayOfString(key: .reviewedSets, value: reviewedIDs)
+        setDate(key: .createdAt, value: Date())
+    }
 }
