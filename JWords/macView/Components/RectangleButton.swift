f4b601f38556f2c426d0fafbf811669ac96d1595
jwmoon <moon910904@gmail.com>
2023-09-08 12:01:00 +0900

    feature: make image optional and add vertical option in RectableButton

--- JWords/macView/Components/RectangleButton.swift
+++ JWords/macView/Components/RectangleButton.swift
@@ -9,30 +9,62 @@ import SwiftUI
 
 struct RectangleButton: View {
     
-    let image: Image
+    let image: Image?
     let title: String
     let onTapped: () -> Void
+    let isVertical: Bool
+    
+    init(image: Image? = nil, title: String,  isVertical: Bool = true, onTapped: @escaping () -> Void) {
+        self.image = image
+        self.title = title
+        self.onTapped = onTapped
+        self.isVertical = isVertical
+    }
     
     var body: some View {
         Button {
             onTapped()
         } label: {
-            VStack {
-                Spacer()
-                image
-                    .resizable()
-                    .frame(width: 20, height: 20)
-                Text(title)
-                Spacer()
+            if isVertical {
+                VStack {
+                    Spacer()
+                    if let image = image {
+                        image
+                            .resizable()
+                            .frame(width: 20, height: 20)
+                    }
+                    Text(title)
+                    Spacer()
+                }
+                .padding(8)
+                .foregroundColor(.black)
+                .cornerRadius(10)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.gray, lineWidth: 1)
+                        .shadow(color: Color.gray.opacity(0.5), radius: 4, x: 0, y: 2)
+                )
+            } else {
+                HStack {
+                    Spacer()
+                    if let image = image {
+                        image
+                            .resizable()
+                            .frame(width: 15, height: 15)
+                    }
+                    Text(title)
+                    Spacer()
+                }
+                .padding(8)
+                .foregroundColor(.black)
+                .cornerRadius(10)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.gray, lineWidth: 1)
+                        .shadow(color: Color.gray.opacity(0.5), radius: 4, x: 0, y: 2)
+                )
             }
-            .padding(8)
-            .foregroundColor(.black)
-            .cornerRadius(10)
-            .overlay(
-                RoundedRectangle(cornerRadius: 10)
-                    .stroke(Color.gray, lineWidth: 1)
-                    .shadow(color: Color.gray.opacity(0.5), radius: 4, x: 0, y: 2)
-            )
+
         }
     }
     
