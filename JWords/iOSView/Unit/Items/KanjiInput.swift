0503bb3464c38764acfc70244bc44251af712852
JW Moon <moon910904@gmail.com>
2023-09-26 18:16:22 +0900

    feature: add KanjiInputField

--- JWords/iOSView/Unit/Items/KanjiInput.swift
+++ JWords/iOSView/Unit/Items/KanjiInput.swift
@@ -6,12 +6,13 @@
 //
 
 import ComposableArchitecture
+import SwiftUI
 
 struct KanjiInput: ReducerProtocol {
 
     struct State: Equatable {
         var text: String = ""
-        var hurigana: EditHuriganaText.State?
+        var hurigana = EditHuriganaText.State(hurigana: "")
         var isEditing: Bool = true
     }
     
@@ -20,6 +21,7 @@ struct KanjiInput: ReducerProtocol {
         case convertToHurigana
         case editText
         case existFound(StudyUnit)
+        case editHuriText(EditHuriganaText.Action)
     }
     
     private let cd = CoreDataClient.shared
@@ -41,12 +43,54 @@ struct KanjiInput: ReducerProtocol {
                 return .none
             case .editText:
                 state.isEditing = true
-                state.hurigana = nil
+                state.hurigana = EditHuriganaText.State(hurigana: "")
                 return .none
             default:
                 return .none
             }
         }
+        Scope(state: \.hurigana, action: /Action.editHuriText) {
+            EditHuriganaText()
+        }
     }
     
+}
+
+struct KanjiInputField: View {
+    
+    let store: StoreOf<KanjiInput>
+    let fontSize: CGFloat
+    
+    var body: some View {
+        WithViewStore(store, observe: { $0 }) { vs in
+            if vs.isEditing {
+                VStack {
+                    InputFieldTitle(title: "단어 (앞면)")
+                    InputFieldTextEditor(text: vs.binding(get: \.text, send: KanjiInput.Action.updateText))
+                    InputFieldButton(label: "후리가나 변환") {
+                        vs.send(.convertToHurigana)
+                    }
+                    .trailingAlignment()
+                }
+            } else {
+                VStack {
+                    InputFieldTitle(title: "후리가나 (앞면)")
+                    ScrollView {
+                        EditableHuriganaText(store: store.scope(
+                            state: \.hurigana,
+                            action: KanjiInput.Action.editHuriText),
+                            fontsize: Constants.Size.UNIT_INPUT_FONT
+                        )
+                        .padding(.horizontal, 5)
+                    }
+                    .frame(height: 100)
+                    .defaultRectangleBackground()
+                    InputFieldButton(label: "단어 수정") {
+                        vs.send(.editText)
+                    }
+                    .trailingAlignment()
+                }
+            }
+        }
+    }
 }
