eb8e7f1f5b4db04b3675ef0302623dd6f77ef186
JW Moon <moon910904@gmail.com>
2023-09-27 08:11:18 +0900

    feature: add button style for Add Unit View

---
+++ JWords/Helper/ButtonStyle.swift
@@ -0,0 +1,32 @@
+//
+//  ButtonStyle.swift
+//  JWords
+//
+//  Created by JW Moon on 2023/09/27.
+//
+
+import SwiftUI
+
+struct InputFieldButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .font(.system(size: Constants.Size.UNIT_INPUT_FONT - 15))
+            .foregroundColor(.black)
+            .padding(5)
+            .defaultRectangleBackground()
+    }
+}
+
+struct InputButtonStyle: ButtonStyle {
+    
+    let isAble: Bool
+    
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .font(.system(size: Constants.Size.UNIT_INPUT_FONT - 15))
+            .foregroundColor(isAble ? .black : .gray)
+            .padding(.vertical, 10)
+            .padding(.horizontal, 20)
+            .defaultRectangleBackground()
+    }
+}
