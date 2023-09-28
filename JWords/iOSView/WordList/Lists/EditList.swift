b4da265523256b538887451a33529ccf574c0a13
JW Moon <moon910904@gmail.com>
2023-09-28 15:01:30 +0900

    refactor: add EditWords Reducer

--- JWords/iOSView/WordList/Lists/EditList.swift
+++ JWords/iOSView/WordList/Lists/EditList.swift
@@ -5,4 +5,31 @@
 //  Created by JW Moon on 2023/09/28.
 //
 
-import Foundation
+import ComposableArchitecture
+import SwiftUI
+
+struct EditWords: ReducerProtocol {
+    struct State: Equatable {
+        var words: IdentifiedArrayOf<EditWord.State>
+        
+        init(words: [StudyUnit], frontType: FrontType) {
+            self.words = IdentifiedArray(uniqueElements: words.map { EditWord.State(unit: $0, frontType: frontType) })
+        }
+        
+    }
+    
+    enum Action: Equatable {
+        case word(id: EditWord.State.ID, action: EditWord.Action)
+    }
+    
+    var body: some ReducerProtocol<State, Action> {
+        Reduce { state, action in
+            switch action {
+            default: return .none
+            }
+        }
+        .forEach(\.words, action: /Action.word(id:action:)) {
+            EditWord()
+        }
+    }
+}
