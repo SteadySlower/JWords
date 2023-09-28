bfede41f923769d7505f5bd0a8025bc39edc963e
JW Moon <moon910904@gmail.com>
2023-09-28 15:24:04 +0900

    refactor: add SelectWords reducer

--- JWords/iOSView/WordList/Lists/SelectList.swift
+++ JWords/iOSView/WordList/Lists/SelectList.swift
@@ -5,5 +5,32 @@
 //  Created by JW Moon on 2023/09/28.
 //
 
-import Foundation
+import ComposableArchitecture
+import SwiftUI
+
+struct SelectWords: ReducerProtocol {
+    struct State: Equatable {
+        var words: IdentifiedArrayOf<SelectionWord.State>
+        
+        init(words: [StudyUnit], frontType: FrontType) {
+            self.words = IdentifiedArray(uniqueElements: words.map { SelectionWord.State(unit: $0, frontType: frontType) })
+        }
+        
+    }
+    
+    enum Action: Equatable {
+        case word(id: SelectionWord.State.ID, action: SelectionWord.Action)
+    }
+    
+    var body: some ReducerProtocol<State, Action> {
+        Reduce { state, action in
+            switch action {
+            default: return .none
+            }
+        }
+        .forEach(\.words, action: /Action.word(id:action:)) {
+            SelectionWord()
+        }
+    }
+}
 
