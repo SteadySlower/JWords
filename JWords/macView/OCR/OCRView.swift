a0c91b312e7f8ccc4103c333d5294eb935367ab0
JW Moon <moon910904@gmail.com>
2023-09-28 13:20:11 +0900

    refactor: add ocr logic in OCRView

--- JWords/macView/OCR/OCRView.swift
+++ JWords/macView/OCR/OCRView.swift
@@ -18,11 +18,54 @@ struct OCR: ReducerProtocol {
     enum Action: Equatable {
         case getImage(GetImageForOCR.Action)
         case ocr(GetWordsFromOCR.Action)
+        case koreanOcrResponse(TaskResult<[OCRResult]>)
+        case japaneseOcrResponse(TaskResult<[OCRResult]>)
+        case koreanOCR(String)
+        case japaneseOCR(String)
     }
+    
+    private let ocrClient: OCRClient = .shared
     
     var body: some ReducerProtocol<State, Action> {
         Reduce { state, action in
-            return .none
+            switch action {
+            case .getImage(let action):
+                switch action {
+                case .imageFetched(let image):
+                    state.ocr = GetWordsFromOCR.State(image: image)
+                    return .merge(
+                        .task {
+                            await .koreanOcrResponse(TaskResult {
+                                try await OCRClient.shared.ocr(from: image, lang: .korean)
+                            })
+                        },
+                        .task {
+                            await .japaneseOcrResponse(TaskResult {
+                                try await OCRClient.shared.ocr(from: image, lang: .japanese)
+                            })
+                        }
+                    )
+                default:
+                    return .none
+                }
+            case .ocr(let action):
+                switch action {
+                case .ocrMarkTapped(let lang, let text):
+                    if lang == .korean {
+                        return .task { .koreanOCR(text) }
+                    } else {
+                        return .task { .japaneseOCR(text) }
+                    }
+                default: return .none
+                }
+            case .japaneseOcrResponse(.success(let result)):
+                state.ocr?.japaneseOcrResult = result
+                return .none
+            case .koreanOcrResponse(.success(let result)):
+                state.ocr?.koreanOcrResult = result
+                return .none
+            default: return .none
+            }
         }
         .ifLet(\.ocr, action: /Action.ocr) {
             GetWordsFromOCR()
