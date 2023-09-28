541d507f287986b9bae59d0943eccdb27d619b89
JW Moon <moon910904@gmail.com>
2023-09-28 15:06:07 +0900

    refactor: add DeleteWords Reducer

--- JWords/iOSView/WordList/Lists/DeleteList.swift
+++ JWords/iOSView/WordList/Lists/DeleteList.swift
@@ -5,4 +5,31 @@
 //  Created by JW Moon on 2023/09/28.
 //
 
-import Foundation
+import ComposableArchitecture
+import SwiftUI
+
+struct DeleteWords: ReducerProtocol {
+    struct State: Equatable {
+        var words: IdentifiedArrayOf<DeleteWord.State>
+        
+        init(words: [StudyUnit], frontType: FrontType) {
+            self.words = IdentifiedArray(uniqueElements: words.map { DeleteWord.State(unit: $0, frontType: frontType) })
+        }
+        
+    }
+    
+    enum Action: Equatable {
+        case word(id: DeleteWord.State.ID, action: DeleteWord.Action)
+    }
+    
+    var body: some ReducerProtocol<State, Action> {
+        Reduce { state, action in
+            switch action {
+            default: return .none
+            }
+        }
+        .forEach(\.words, action: /Action.word(id:action:)) {
+            DeleteWord()
+        }
+    }
+}
