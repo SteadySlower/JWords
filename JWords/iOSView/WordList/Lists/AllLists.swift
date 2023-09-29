eff87275f597c4f0daacbefa387455f7705b2655
JW Moon <moon910904@gmail.com>
2023-09-29 13:08:03 +0900

    feature: add state changing API in SwitchBetweenLists

--- JWords/iOSView/WordList/Lists/AllLists.swift
+++ JWords/iOSView/WordList/Lists/AllLists.swift
@@ -15,10 +15,12 @@ enum ListType {
 struct SwitchBetweenList: ReducerProtocol {
     struct State: Equatable {
         
+        private let _units: [StudyUnit]
         private var units: [StudyUnit]
+        private var filter: UnitFilter
         var type: ListType
-        private let frontType: FrontType
-        private let isLocked: Bool
+        private var frontType: FrontType
+        private var isLocked: Bool
         
         var study: StudyWords.State?
         var edit: EditWords.State?
@@ -26,17 +28,67 @@ struct SwitchBetweenList: ReducerProtocol {
         var delete: DeleteWords.State?
         
         init(units: [StudyUnit], frontType: FrontType, isLocked: Bool) {
+            self._units = units
             self.units = units
+            self.filter = .all
             self.type = .study
             self.frontType = frontType
             self.isLocked = isLocked
             self.study = StudyWords.State(words: units, frontType: frontType, isLocked: isLocked)
         }
         
+        private mutating func clear() {
+            study = nil
+            edit = nil
+            select = nil
+            delete = nil
+            type = .study
+        }
+        
         mutating func shuffle() {
-            guard type == .study else { return }
+            clear()
             units = units.shuffled()
             study = StudyWords.State(words: units, frontType: frontType, isLocked: isLocked)
+        }
+        
+        mutating func setFrontType(_ frontType: FrontType) {
+            clear()
+            self.frontType = frontType
+            study = StudyWords.State(words: units, frontType: frontType, isLocked: isLocked)
+        }
+        
+        mutating func setFilter(_ filter: UnitFilter) {
+            clear()
+            switch filter {
+            case .all:
+                units = _units
+            case .excludeSuccess:
+                units = _units.filter { $0.studyState != .success }
+            case .onlyFail:
+                units = _units.filter { $0.studyState == .fail }
+            }
+            study = StudyWords.State(words: units, frontType: frontType, isLocked: isLocked)
+        }
+        
+        mutating func setIsLocked(_ isLocked: Bool) {
+            clear()
+            self.isLocked = isLocked
+            study = StudyWords.State(words: units, frontType: frontType, isLocked: isLocked)
+        }
+        
+        mutating func setListType(_ type: ListType) {
+            clear()
+            self.type = type
+            switch type {
+            case .study:
+                study = StudyWords.State(words: units, frontType: frontType, isLocked: isLocked)
+            case .edit:
+                edit = EditWords.State(words: units, frontType: frontType)
+            case .select:
+                select = SelectWords.State(words: units, frontType: frontType)
+            case .delete:
+                delete = DeleteWords.State(words: units, frontType: frontType)
+            }
         }
     }
     
