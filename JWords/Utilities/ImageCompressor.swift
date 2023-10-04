b6a86d06b235c1f1c7410f1229202eff31c3fa83
JW Moon <moon910904@gmail.com>
2023-10-04 14:24:01 +0900

    refactor: remove redundant error

--- JWords/Utilities/ImageCompressor.swift
+++ JWords/Utilities/ImageCompressor.swift
@@ -18,14 +18,14 @@ class ImageCompressorImpl: ImageCompressor {
     func compressImageToJPEG(image: InputImageType) throws -> Data {
         #if os(iOS)
         guard let jpegData = image.jpegData(compressionQuality: 0.5) else {
-            throw AppError.ImageCompressor.imageToDataFail
+            throw AppError.unknown
         }
         return jpegData
         #elseif os(macOS)
         let cgImage = image.cgImage(forProposedRect: nil, context: nil, hints: nil)!
         let bitmapRep = NSBitmapImageRep(cgImage: cgImage)
         guard let jpegData = bitmapRep.representation(using: NSBitmapImageRep.FileType.jpeg, properties: [:]) else {
-            throw AppError.ImageCompressor.imageToDataFail
+            throw AppError.unknown
         }
         return jpegData
         #endif
