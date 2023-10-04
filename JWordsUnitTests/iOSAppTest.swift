0188efef383b0e03acebf8c6b3b0d251781fec1e
JW Moon <moon910904@gmail.com>
2023-10-04 10:37:27 +0900

    test: add iOSAppTest

--- JWordsUnitTests/iOSAppTest.swift
+++ JWordsUnitTests/iOSAppTest.swift
@@ -5,4 +5,66 @@
 //  Created by JW Moon on 2023/10/04.
 //
 
-import Foundation
+import ComposableArchitecture
+import XCTest
+
+@testable import JWords
+
+@MainActor
+final class TodosTests: XCTestCase {
+    func testChangeTab() async {
+        let store = TestStore(
+            initialState: iOSApp.State(),
+            reducer: iOSApp()
+        )
+        
+        XCTAssertEqual(store.state.selectedTab, .today)
+        
+        await store.send(.tabChanged(.home)) {
+            $0.selectedTab = .home
+            $0.homeList = HomeList.State()
+            $0.todayList = nil
+        }
+        
+        await store.send(.tabChanged(.kanji)) {
+            $0.selectedTab = .kanji
+            $0.kanjiList = KanjiList.State()
+            $0.homeList = nil
+        }
+        
+        await store.send(.tabChanged(.ocr)) {
+            $0.selectedTab = .ocr
+            $0.ocr = AddUnitWithOCR.State()
+            $0.kanjiList = nil
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
