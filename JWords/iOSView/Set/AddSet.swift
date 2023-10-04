ac5a6bfe2797c7e8da9e8544bd475521dcfcd3dd
JW Moon <moon910904@gmail.com>
2023-10-04 12:35:59 +0900

    feature: add AddSet

--- JWords/iOSView/Set/AddSet.swift
+++ JWords/iOSView/Set/AddSet.swift
@@ -5,4 +5,87 @@
 //  Created by JW Moon on 2023/10/04.
 //
 
-import Foundation
+import ComposableArchitecture
+import SwiftUI
+
+struct AddSet: ReducerProtocol {
+    
+    struct State: Equatable {
+        var inputSet: InputSet.State = .init()
+        
+        var ableToAdd: Bool {
+            !inputSet.title.isEmpty
+        }
+    }
+    
+    enum Action: Equatable {
+        case inputSet(InputSet.Action)
+        case add
+        case cancel
+        case added
+    }
+    
+    @Dependency(\.studySetClient) var setClient
+    
+    var body: some ReducerProtocol<State, Action> {
+        Reduce { state, action in
+            switch action {
+            case .add:
+                let input = StudySetInput(
+                    title: state.inputSet.title,
+                    isAutoSchedule: true,
+                    preferredFrontType: state.inputSet.frontType
+                )
+                try! setClient.insert(input)
+                return .task { .added }
+            default: return .none
+            }
+        }
+        Scope(
+            state: \.inputSet,
+            action: /Action.inputSet,
+            child: { InputSet() }
+        )
+    }
+    
+}
+
+struct AddSetView: View {
+    
+    let store: StoreOf<AddSet>
+    
+    var body: some View {
+        WithViewStore(store, observe: { $0 }) { vs in
+            VStack(spacing: 30) {
+                InputSetView(store: store.scope(
+                    state: \.inputSet,
+                    action: AddSet.Action.inputSet)
+                )
+                HStack {
+                    Spacer()
+                    Button("취소") {
+                        vs.send(.cancel)
+                    }
+                    .buttonStyle(InputButtonStyle())
+                    Spacer()
+                    Button("추가") {
+                        vs.send(.add)
+                    }
+                    .buttonStyle(InputButtonStyle(isAble: vs.ableToAdd))
+                    .disabled(!vs.ableToAdd)
+                    Spacer()
+                }
+            }
+            .padding(.horizontal, 10)
+            .presentationDetents([.medium])
+        }
+    }
+}
+
+#Preview {
+    AddSetView(store: Store(
+        initialState: AddSet.State(),
+        reducer: AddSet())
+    )
+}
+
