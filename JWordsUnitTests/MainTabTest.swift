f7bcb96f7d665d043571bcaa69efcf3fc3f54ed6
JW Moon <moon910904@gmail.com>
2023-10-05 12:31:24 +0900

    refactor: change test name

--- JWordsUnitTests/MainTabTest.swift
+++ JWordsUnitTests/MainTabTest.swift
@@ -1,5 +1,5 @@
 //
-//  iOSAppTest.swift
+//  MainTabTest.swift
 //  JWordsUnitTests
 //
 //  Created by JW Moon on 2023/10/04.
@@ -11,31 +11,25 @@ import XCTest
 @testable import JWords
 
 @MainActor
-final class iOSAppTest: XCTestCase {
+final class MainTabTest: XCTestCase {
     func testChangeTab() async {
         let store = TestStore(
-            initialState: iOSApp.State(),
-            reducer: iOSApp()
+            initialState: MainTab.State(),
+            reducer: MainTab()
         )
         
         XCTAssertEqual(store.state.selectedTab, .today)
         
         await store.send(.tabChanged(.home)) {
             $0.selectedTab = .home
-            $0.homeList = HomeList.State()
-            $0.todayList = nil
         }
         
         await store.send(.tabChanged(.kanji)) {
             $0.selectedTab = .kanji
-            $0.kanjiList = KanjiList.State()
-            $0.homeList = nil
         }
         
         await store.send(.tabChanged(.ocr)) {
             $0.selectedTab = .ocr
-            $0.ocr = AddUnitWithOCR.State()
-            $0.kanjiList = nil
         }
     }
 }
