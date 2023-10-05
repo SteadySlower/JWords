adf56d48d0fe1e7673ad0d17d2e6ee51b01cad3f
JW Moon <moon910904@gmail.com>
2023-10-05 16:44:07 +0900

    test: add TodayListTest

--- JWordsUnitTests/TodayListTest.swift
+++ JWordsUnitTests/TodayListTest.swift
@@ -5,4 +5,47 @@
 //  Created by JW Moon on 2023/10/05.
 //
 
-import Foundation
+import ComposableArchitecture
+import XCTest
+
+@testable import JWords
+
+@MainActor
+final class TodayListTest: XCTestCase {
+    func testOnAppear() async {
+        let store = TestStore(
+            initialState: TodayList.State(),
+            reducer: TodayList()
+        )
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
