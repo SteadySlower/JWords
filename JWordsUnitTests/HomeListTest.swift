22c59d0283eae7ba43c976fe63771508e2022b1b
JW Moon <moon910904@gmail.com>
2023-10-06 18:06:39 +0900

    refactor: refactor HomeListTest

--- JWordsUnitTests/HomeListTest.swift
+++ JWordsUnitTests/HomeListTest.swift
@@ -25,6 +25,10 @@ final class HomeListTest: XCTestCase {
         let store = TestStore(
             initialState: HomeList.State(),
             reducer: HomeList())
+        {
+            $0.studySetClient.fetch = { bool in bool ? .mockIncludingClosed : .mock }
+            $0.studyUnitClient.fetch = { _ in .mock }
+        }
         
         await store.send(.onAppear) {
             $0.sets = .mock
@@ -34,19 +38,13 @@ final class HomeListTest: XCTestCase {
     }
     
     func testOnAppear() async {
-        let store = TestStore(
-            initialState: HomeList.State(),
-            reducer: HomeList())
-
-        await store.send(.onAppear) {
-            $0.sets = .mock
-        }
+        _ = await setStore()
     }
     
     func testHomeCellTapped() async {
         let store = await setStore()
         
-        let tappedSet = [StudySet].mock[0]
+        let tappedSet = store.state.sets[0]
         
         await store.send(.homeCellTapped(tappedSet)) {
             $0.studyUnitsInSet = StudyUnitsInSet.State(set: tappedSet, units: .mock)
