f40e7f5ca337ed53ef553b353ce6ae68dcc4713b
JW Moon <moon910904@gmail.com>
2023-10-14 22:07:40 +0900

    test: add AddUnitTest

---
+++ JWordsUnitTests/AddUnitTest.swift
@@ -0,0 +1,17 @@
+//
+//  AddUnitTest.swift
+//  JWordsUnitTests
+//
+//  Created by JW Moon on 2023/10/14.
+//
+
+import ComposableArchitecture
+import XCTest
+
+@testable import JWords
+
+@MainActor
+final class AddUnitTest: XCTestCase {
+    
+}
+
