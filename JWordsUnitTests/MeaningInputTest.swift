0afc5d6ba47c8882ade5aee664694180eb708bae
JW Moon <moon910904@gmail.com>
2023-10-15 14:56:23 +0900

    test: add meaningInputTest

---
+++ JWordsUnitTests/MeaningInputTest.swift
@@ -0,0 +1,17 @@
+//
+//  MeaningInputTest.swift
+//  JWordsUnitTests
+//
+//  Created by JW Moon on 10/15/23.
+//
+
+import ComposableArchitecture
+import XCTest
+
+@testable import JWords
+
+@MainActor
+final class MeaningInputTest: XCTestCase {
+    
+}
+
