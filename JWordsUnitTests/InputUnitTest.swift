05c02d20132d27b8780585fb4ca3026b3619931e
JW Moon <moon910904@gmail.com>
2023-10-15 12:46:25 +0900

    test: add InputUnitTest

---
+++ JWordsUnitTests/InputUnitTest.swift
@@ -0,0 +1,16 @@
+//
+//  InputUnitTest.swift
+//  JWordsUnitTests
+//
+//  Created by JW Moon on 10/15/23.
+//
+
+import ComposableArchitecture
+import XCTest
+
+@testable import JWords
+
+@MainActor
+final class InputUnitTest: XCTestCase {
+    
+}
