432090bee73a78b4d48c9a59ee39dff445ab2ecc
JW Moon <moon910904@gmail.com>
2023-10-15 11:48:49 +0900

    test: add testInit

--- JWordsUnitTests/EditUnitTest.swift
+++ JWordsUnitTests/EditUnitTest.swift
@@ -13,4 +13,19 @@ import XCTest
 @MainActor
 final class EditUnitTest: XCTestCase {
     
+    func testInit() async {
+        let unit: StudyUnit = .testMock
+        
+        let store = TestStore(
+            initialState: EditUnit.State(unit: unit),
+            reducer: { EditUnit() }
+        )
+        
+        XCTAssert(store.state.unit == unit)
+        XCTAssert(store.state.inputUnit.kanjiInput.text == HuriganaConverter.shared.huriToKanjiText(from: unit.kanjiText))
+        XCTAssert(store.state.inputUnit.kanjiInput.hurigana.hurigana == EditHuriganaText.State(hurigana: unit.kanjiText).hurigana)
+        XCTAssert(store.state.inputUnit.kanjiInput.isEditing == false)
+        XCTAssert(store.state.inputUnit.meaningInput.text == unit.meaningText)
+    }
+    
 }
