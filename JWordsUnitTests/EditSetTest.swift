b7740d4a17a4fcbca7ea29edbd449a713b9c083f
JW Moon <moon910904@gmail.com>
2023-10-15 15:41:43 +0900

    test: add EditSetTest

---
+++ JWordsUnitTests/EditSetTest.swift
@@ -0,0 +1,16 @@
+//
+//  EditSetTest.swift
+//  JWordsUnitTests
+//
+//  Created by JW Moon on 10/15/23.
+//
+
+import ComposableArchitecture
+import XCTest
+
+@testable import JWords
+
+@MainActor
+final class EditSetTest: XCTestCase {
+    
+}
