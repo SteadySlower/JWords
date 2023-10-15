26f8ebe1561a11dfd76eebf608ae4a47ed2e5854
JW Moon <moon910904@gmail.com>
2023-10-15 15:19:06 +0900

    test: add test_onAppear

--- JWordsUnitTests/KanjiListTest.swift
+++ JWordsUnitTests/KanjiListTest.swift
@@ -13,4 +13,23 @@ import XCTest
 @MainActor
 final class KanjiListTest: XCTestCase {
     
+    func test_onAppear() async {
+        let fetchKanjiCount = KanjiList.NUMBER_OF_KANJI_IN_A_PAGE
+        let fetched: [Kanji] = .testMock(count: fetchKanjiCount)
+        
+        let store = TestStore(
+            initialState: KanjiList.State(),
+            reducer: { KanjiList() },
+            withDependencies: {
+                $0.kanjiClient.fetch = { _ in fetched }
+            }
+        )
+        
+        await store.send(.onAppear)
+        
+        await store.receive(.fetchKanjis) {
+            $0.kanjis.append(contentsOf: fetched)
+        }
+    }
+    
 }
