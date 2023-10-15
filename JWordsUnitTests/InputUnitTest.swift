7db25e0f37b4217d2edf2b9529048d6481124ddd
JW Moon <moon910904@gmail.com>
2023-10-15 12:52:38 +0900

    test: add test for testKanjiInputHuriganaUpdatedAlreadyExist

--- JWordsUnitTests/InputUnitTest.swift
+++ JWordsUnitTests/InputUnitTest.swift
@@ -13,4 +13,31 @@ import XCTest
 @MainActor
 final class InputUnitTest: XCTestCase {
     
+    func testKanjiInputHuriganaUpdatedAlreadyExist() async {
+        let alreadyExist: StudyUnit = .testMock
+        let store = TestStore(
+            initialState: InputUnit.State(),
+            reducer: { InputUnit() },
+            withDependencies: {
+                $0.studyUnitClient.checkIfExist = { _ in alreadyExist }
+            }
+        )
+        
+        await store.send(.kanjiInput(.huriganaUpdated(Random.string)))
+        await store.receive(.alreadyExist(alreadyExist))
+    }
+    
+    func testKanjiInputHuriganaUpdatedAlreadyExistNil() async {
+        let store = TestStore(
+            initialState: InputUnit.State(),
+            reducer: { InputUnit() },
+            withDependencies: {
+                $0.studyUnitClient.checkIfExist = { _ in nil }
+            }
+        )
+        
+        await store.send(.kanjiInput(.huriganaUpdated(Random.string)))
+        await store.receive(.alreadyExist(nil))
+    }
+    
 }
