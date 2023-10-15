01bdeb511583780b3b9f924266973c7c35f2b504
JW Moon <moon910904@gmail.com>
2023-10-15 15:40:44 +0900

    test: add test_add

--- JWordsUnitTests/AddSetTest.swift
+++ JWordsUnitTests/AddSetTest.swift
@@ -13,4 +13,19 @@ import XCTest
 @MainActor
 final class AddSetTest: XCTestCase {
     
+    func test_add() async {
+        let set: StudySet = .testMock
+        let store = TestStore(
+            initialState: AddSet.State(),
+            reducer: { AddSet() },
+            withDependencies: {
+                $0.studySetClient.insert = { _ in set }
+            }
+        )
+        
+        await store.send(.add)
+        
+        await store.receive(.added(set))
+    }
+    
 }
