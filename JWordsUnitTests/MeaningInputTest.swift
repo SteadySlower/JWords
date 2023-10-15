0fe06890d756dd8df14bc118f0651f9628f5d2a5
JW Moon <moon910904@gmail.com>
2023-10-15 14:57:48 +0900

    test: add testUpdateText

--- JWordsUnitTests/MeaningInputTest.swift
+++ JWordsUnitTests/MeaningInputTest.swift
@@ -13,5 +13,18 @@ import XCTest
 @MainActor
 final class MeaningInputTest: XCTestCase {
     
+    func testUpdateText() async {
+        let store = TestStore(
+            initialState: MeaningInput.State(),
+            reducer: { MeaningInput() }
+        )
+        
+        let text = Random.string
+        
+        await store.send(.updateText(text)) {
+            $0.text = text
+        }
+    }
+    
 }
 
