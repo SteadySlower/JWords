5f4705c2b1acb56a267b63897618bd3e365b3c29
jwmoon <moon910904@gmail.com>
2023-10-16 09:09:02 +0900

    test: add OCRTest

---
+++ JWordsUnitTests/OCRTest.swift
@@ -0,0 +1,16 @@
+//
+//  OCRTest.swift
+//  JWordsUnitTests
+//
+//  Created by Jong Won Moon on 10/16/23.
+//
+
+import ComposableArchitecture
+import XCTest
+
+@testable import JWords
+
+@MainActor
+final class OCRTest: XCTestCase {
+    
+}
