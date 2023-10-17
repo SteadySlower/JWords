e22011d46fc00fc6e52465486b810f4a3f86a1fa
jwmoon <moon910904@gmail.com>
2023-10-17 12:18:57 +0900

    test: add GetImageForOCRTest

---
+++ JWordsUnitTests/GetImageForOCRTest.swift
@@ -0,0 +1,16 @@
+//
+//  GetImageForOCRTest.swift
+//  JWordsUnitTests
+//
+//  Created by Jong Won Moon on 10/17/23.
+//
+
+import ComposableArchitecture
+import XCTest
+
+@testable import JWords
+
+@MainActor
+final class GetImageForOCRTest: XCTestCase {
+    
+}
