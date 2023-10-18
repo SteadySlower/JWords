085a326a6bc782d3e23228a97e716cf142ef0c1a
jwmoon <moon910904@gmail.com>
2023-10-18 17:20:35 +0900

    test: add StudySettingTest

---
+++ JWordsUnitTests/GetTextsFromOCRTest.swift
@@ -0,0 +1,16 @@
+//
+//  GetTextsFromOCRTest.swift
+//  JWordsUnitTests
+//
+//  Created by Jong Won Moon on 10/18/23.
+//
+
+import ComposableArchitecture
+import XCTest
+
+@testable import JWords
+
+@MainActor
+final class GetTextsFromOCRTest: XCTestCase {
+    
+}
---
+++ JWordsUnitTests/StudySettingTest.swift
@@ -0,0 +1,16 @@
+//
+//  StudySettingTest.swift
+//  JWordsUnitTests
+//
+//  Created by Jong Won Moon on 10/18/23.
+//
+
+import ComposableArchitecture
+import XCTest
+
+@testable import JWords
+
+@MainActor
+final class StudySettingTest: XCTestCase {
+    
+}
