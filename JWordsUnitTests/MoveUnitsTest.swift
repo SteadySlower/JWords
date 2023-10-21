c413882d717e1e9769a48e89eb027ff3e62d06c5
JW Moon <moon910904@gmail.com>
2023-10-21 10:58:47 +0900

    test: add MoveUnitsTest

---
+++ JWordsUnitTests/MoveUnitsTest.swift
@@ -0,0 +1,18 @@
+//
+//  MoveUnitsTest.swift
+//  JWordsUnitTests
+//
+//  Created by JW Moon on 10/21/23.
+//
+
+import ComposableArchitecture
+import XCTest
+
+@testable import JWords
+
+@MainActor
+final class MoveUnitsTest: XCTestCase {
+    
+    
+    
+}
