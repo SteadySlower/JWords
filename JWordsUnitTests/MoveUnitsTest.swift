ba67b691d8173344cf16d2980d44d6eeccd20ff0
JW Moon <moon910904@gmail.com>
2023-10-21 11:04:37 +0900

    fix: remove isLoading

--- JWordsUnitTests/MoveUnitsTest.swift
+++ JWordsUnitTests/MoveUnitsTest.swift
@@ -13,6 +13,25 @@ import XCTest
 @MainActor
 final class MoveUnitsTest: XCTestCase {
     
-    
+    func test_onAppear() async {
+        
+        let sets: [StudySet] = .testMock
+        
+        let store = TestStore(
+            initialState: MoveUnits.State(
+                fromSet: .testMock,
+                isReviewSet: Bool.random(),
+                toMoveUnits: .testMock
+            ),
+            reducer: { MoveUnits() },
+            withDependencies: {
+                $0.studySetClient.fetch = { _ in sets }
+            }
+        )
+        
+        await store.send(.onAppear) {
+            $0.sets = sets
+        }
+    }
     
 }
