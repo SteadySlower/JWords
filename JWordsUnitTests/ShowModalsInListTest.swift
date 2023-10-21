cb0a021c03e6cad55d5d93848b7be6dabf82ee6a
JW Moon <moon910904@gmail.com>
2023-10-21 11:47:38 +0900

    test: add test_showAddUnitModal

--- JWordsUnitTests/ShowModalsInListTest.swift
+++ JWordsUnitTests/ShowModalsInListTest.swift
@@ -23,16 +23,37 @@ final class ShowModalsInListTest: XCTestCase {
     }
     
     func test_showEditSetModal_false() async {
-        let editSet = EditSet.State(.testMock)
         let store = TestStore(
             initialState: ShowModalsInList.State(
-                editSet: editSet
+                editSet: .init(.testMock)
             ),
             reducer: { ShowModalsInList() }
         )
         
         await store.send(.showEditSetModal(false)) {
             $0.editSet = nil
+        }
+    }
+    
+    func test_showAddUnitModal_true() async {
+        let store = TestStore(
+            initialState: ShowModalsInList.State(),
+            reducer: { ShowModalsInList() }
+        )
+        
+        await store.send(.showAddUnitModal(true))
+    }
+    
+    func test_showAddUnitModal_false() async {
+        let store = TestStore(
+            initialState: ShowModalsInList.State(
+                addUnit: .init()
+            ),
+            reducer: { ShowModalsInList() }
+        )
+        
+        await store.send(.showAddUnitModal(false)) {
+            $0.addUnit = nil
         }
     }
     
