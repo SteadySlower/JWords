e51e2df92eba86377374a6e8dfb88857c4169e91
JW Moon <moon910904@gmail.com>
2023-10-21 13:55:52 +0900

    test: add EditUnitsTest

---
+++ JWordsUnitTests/EditUnitsTest.swift
@@ -0,0 +1,14 @@
+//
+//  EditUnitsTest.swift
+//  JWordsUnitTests
+//
+//  Created by JW Moon on 10/21/23.
+//
+
+import ComposableArchitecture
+import XCTest
+
+@testable import JWords
+
+@MainActor
+final class EditUnitsTest: XCTestCase {}
