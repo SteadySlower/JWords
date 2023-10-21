3d30805f8e20e37ed2c5cd9fa9a4590a67516f36
JW Moon <moon910904@gmail.com>
2023-10-21 13:28:19 +0900

    test: add SwitchBetweenListTest

---
+++ JWordsUnitTests/SwitchBetweenListTest.swift
@@ -0,0 +1,14 @@
+//
+//  SwitchBetweenListTest.swift
+//  JWordsUnitTests
+//
+//  Created by JW Moon on 10/21/23.
+//
+
+import ComposableArchitecture
+import XCTest
+
+@testable import JWords
+
+@MainActor
+final class SwitchBetweenListTest: XCTestCase {}
