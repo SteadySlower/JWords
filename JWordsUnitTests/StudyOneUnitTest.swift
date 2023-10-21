d035bb7ad0562d5c757d4a3642969ad67d0e203b
JW Moon <moon910904@gmail.com>
2023-10-21 13:21:16 +0900

    test: add test_cellDrag_left

--- JWordsUnitTests/StudyOneUnitTest.swift
+++ JWordsUnitTests/StudyOneUnitTest.swift
@@ -59,4 +59,39 @@ final class StudyOneUnitTest: XCTestCase {
         await store.send(.cellDoubleTapped)
     }
     
+    func test_cellDrag_left() async {
+        let testMock: StudyUnit = [.undefinedTestMock, .failTestMock].randomElement()!
+        
+        let store = TestStore(
+            initialState: StudyOneUnit.State(
+                unit: testMock
+            ),
+            reducer: { StudyOneUnit() },
+            withDependencies: {
+                $0.studyUnitClient.studyState = { _, state in state }
+            }
+        )
+        
+        await store.send(.cellDrag(direction: .left)) {
+            $0.studyState = .success
+        }
+    }
+    
+    func test_cellDrag_left_locked() async {
+        let testMock: StudyUnit = [.undefinedTestMock, .failTestMock].randomElement()!
+        
+        let store = TestStore(
+            initialState: StudyOneUnit.State(
+                unit: testMock,
+                isLocked: true
+            ),
+            reducer: { StudyOneUnit() },
+            withDependencies: {
+                $0.studyUnitClient.studyState = { _, state in state }
+            }
+        )
+        
+        await store.send(.cellDrag(direction: .left))
+    }
+    
 }
