bcdb8d114582bc3204a4111865f985f2a149343b
JW Moon <moon910904@gmail.com>
2023-10-22 12:47:19 +0900

    test: add TodaySelectionTest

---
+++ JWordsUnitTests/TodaySelectionTest.swift
@@ -0,0 +1,14 @@
+//
+//  TodaySelectionTest.swift
+//  JWordsUnitTests
+//
+//  Created by JW Moon on 10/22/23.
+//
+
+import ComposableArchitecture
+import XCTest
+
+@testable import JWords
+
+@MainActor
+final class TodaySelectionTest: XCTestCase {}
