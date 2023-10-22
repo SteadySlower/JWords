86b1ed09c838d4afa96d11702a70c85e74bf9dfb
JW Moon <moon910904@gmail.com>
2023-10-22 10:11:43 +0900

    test: add test_lists_toEditUnitSelected

--- JWordsUnitTests/StudyUnitsTest.swift
+++ JWordsUnitTests/StudyUnitsTest.swift
@@ -11,4 +11,20 @@ import XCTest
 @testable import JWords
 
 @MainActor
-final class StudyUnitsTest: XCTestCase {}
+final class StudyUnitsTest: XCTestCase {
+    
+    func test_lists_toEditUnitSelected() async {
+        let store = TestStore(
+            initialState: StudyUnits.State(
+                units: .testMock
+            ),
+            reducer: { StudyUnits() }
+        )
+        
+        let unit: StudyUnit = .testMock
+        await store.send(.lists(.toEditUnitSelected(unit))) {
+            $0.modals.setEditUnitModal(unit)
+        }
+    }
+    
+}
