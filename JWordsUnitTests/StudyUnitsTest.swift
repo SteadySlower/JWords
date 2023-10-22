f855828cdbc2366fed3ab0bb06d16768b8aeff80
JW Moon <moon910904@gmail.com>
2023-10-22 10:08:05 +0900

    test: add StudyUnitsTest

---
+++ JWordsUnitTests/StudyUnitsTest.swift
@@ -0,0 +1,14 @@
+//
+//  StudyUnitsTest.swift
+//  JWordsUnitTests
+//
+//  Created by JW Moon on 10/22/23.
+//
+
+import ComposableArchitecture
+import XCTest
+
+@testable import JWords
+
+@MainActor
+final class StudyUnitsTest: XCTestCase {}
