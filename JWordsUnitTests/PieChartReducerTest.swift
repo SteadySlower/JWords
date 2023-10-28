c421c74e6d873050eead2a8400f8bb0aa7075ae1
JW Moon <moon910904@gmail.com>
2023-10-28 11:28:14 +0900

    test: add PieChartReducerTest

---
+++ JWordsUnitTests/PieChartReducerTest.swift
@@ -0,0 +1,14 @@
+//
+//  PieChartReducerTest.swift
+//  JWordsUnitTests
+//
+//  Created by JW Moon on 10/28/23.
+//
+
+import ComposableArchitecture
+import XCTest
+
+@testable import JWords
+
+@MainActor
+final class PieChartReducerTest: XCTestCase {}
