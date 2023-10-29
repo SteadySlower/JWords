cfc5601b92139328d8c387f923f20d858e688153
JW Moon <moon910904@gmail.com>
2023-10-29 21:34:10 +0900

    test: add test_startAnimation

--- JWordsUnitTests/PieChartReducerTest.swift
+++ JWordsUnitTests/PieChartReducerTest.swift
@@ -11,4 +11,35 @@ import XCTest
 @testable import JWords
 
 @MainActor
-final class PieChartReducerTest: XCTestCase {}
+final class PieChartReducerTest: XCTestCase {
+    
+    func test_startAnimation() async {
+        let clock = TestClock()
+        let percentage = Float.random(in: 0..<1.0)
+        
+        let store = TestStore(
+            initialState: PieChartReducer.State(
+                _percentage: percentage
+            ),
+            reducer: { PieChartReducer() },
+            withDependencies: {
+                $0.continuousClock = clock
+            }
+        )
+        
+        await store.send(.startAnimation)
+        
+        for _ in 0..<200 {
+            await clock.advance(by: .seconds(0.005))
+            await store.receive(.addToDisplayPercentage(percentage / 200)) {
+                $0.displayPercentage += percentage / 200
+                if $0.displayPercentage > $0._percentage {
+                    $0.displayPercentage = $0._percentage
+                }
+            }
+        }
+        
+        await clock.run()
+    }
+    
+}
