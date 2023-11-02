2cb8d9405f9eef723b2415a36b6d6145dd4617bc
jwmoon <moon910904@gmail.com>
2023-11-02 16:54:49 +0900

    test: add SelectionStudySetTest

---
+++ JWordsUnitTests/SelectionStudySetTest.swift
@@ -0,0 +1,16 @@
+//
+//  SelectionStudySetTest.swift
+//  JWordsUnitTests
+//
+//  Created by Jong Won Moon on 11/2/23.
+//
+
+import ComposableArchitecture
+import XCTest
+
+@testable import JWords
+
+@MainActor
+final class SelectionStudySetTest: XCTestCase {
+    
+}
