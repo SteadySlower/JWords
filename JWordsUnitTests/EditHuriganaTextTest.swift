26a15bcadc1a91bfc544ba837fb7e4e076d763b4
jwmoon <moon910904@gmail.com>
2023-11-03 16:33:55 +0900

    test: add test_onGanaUpdated

--- JWordsUnitTests/EditHuriganaTextTest.swift
+++ JWordsUnitTests/EditHuriganaTextTest.swift
@@ -13,4 +13,25 @@ import XCTest
 @MainActor
 final class EditHuriganaTextTest: XCTestCase {
     
+    func test_onGanaUpdated() async {
+        let toBeUpdatedHuri = Huri(id: Random.string, huriString: Random.string)
+        var mockHuris = [toBeUpdatedHuri]
+        (0..<Random.int(from: 0, to: 100)).forEach { _ in
+            mockHuris.append(Huri(id: Random.string, huriString: Random.string))
+        }
+        mockHuris = mockHuris.shuffled()
+        let updatedHuri = Huri(id: toBeUpdatedHuri.id, huriString: Random.string)
+        
+        let store = TestStore(
+            initialState: EditHuriganaText.State(huris: mockHuris),
+            reducer: { EditHuriganaText() }
+        )
+        
+        await store.send(.onGanaUpdated(updatedHuri)) {
+            $0.updateHuri(updatedHuri)
+        }
+        
+        await store.receive(.onHuriUpdated)
+    }
+    
 }
