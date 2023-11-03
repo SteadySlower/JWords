0d4ff1a855e38a79575e94be484ed40cc4354fb3
jwmoon <moon910904@gmail.com>
2023-11-03 16:04:30 +0900

    test: add two testUpdateID tests

--- JWordsUnitTests/SelectionStudySetTest.swift
+++ JWordsUnitTests/SelectionStudySetTest.swift
@@ -30,12 +30,47 @@ final class SelectionStudySetTest: XCTestCase {
     
     func testUpdateID() async {
         let sets: [StudySet] = .testMock
+        let unitCount = Random.int(from: 0, to: 9999)
+        let id = sets.randomElement()!.id
+        
         let store = TestStore(
             initialState: SelectStudySet.State(
-                sets:
+                sets: sets
+            ),
+            reducer: { SelectStudySet() },
+            withDependencies: {
+                $0.studySetClient.countUnits = { _ in unitCount }
+            }
+        )
+        
+        await store.send(.updateID(id)) {
+            $0.selectedID = id
+            $0.unitCount = unitCount
+        }
+        
+        await store.receive(.idUpdated(store.state.selectedSet))
+    }
+    
+    func testUpdateID_toNil() async {
+        let sets: [StudySet] = .testMock
+        let unitCount = Random.int(from: 0, to: 9999)
+        let id = sets.randomElement()!.id
+        
+        let store = TestStore(
+            initialState: SelectStudySet.State(
+                sets: sets,
+                selectedID: id,
+                unitCount: unitCount
             ),
             reducer: { SelectStudySet() }
         )
+        
+        await store.send(.updateID(nil)) {
+            $0.selectedID = nil
+            $0.unitCount = nil
+        }
+        
+        await store.receive(.idUpdated(nil))
     }
     
 }
