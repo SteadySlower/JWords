140f6a3fe3315e61e3e35dcf151f0cdfe8643761
jwmoon <moon910904@gmail.com>
2023-11-03 16:16:33 +0900

    test: add EditHuriganaTextTest

---
+++ JWordsUnitTests/EditHuriganaTextTest.swift
@@ -0,0 +1,16 @@
+//
+//  EditHuriganaTextTest.swift
+//  JWordsUnitTests
+//
+//  Created by Jong Won Moon on 11/3/23.
+//
+
+import ComposableArchitecture
+import XCTest
+
+@testable import JWords
+
+@MainActor
+final class EditHuriganaTextTest: XCTestCase {
+    
+}
