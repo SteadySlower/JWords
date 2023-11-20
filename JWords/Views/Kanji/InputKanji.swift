8d3c8ef19d1309fec382bf0d34d291c7294bc271
jwmoon <moon910904@gmail.com>
2023-11-20 15:09:17 +0900

    feature: add string states in InputKanji reducer

--- JWords/Views/Kanji/InputKanji.swift
+++ JWords/Views/Kanji/InputKanji.swift
@@ -10,16 +10,34 @@ import ComposableArchitecture
 
 struct InputKanji: Reducer {
     struct State: Equatable {
-        
+        var kanji: String
+        var meaning: String
+        var ondoku: String
+        var kundoku: String
     }
     
     enum Action: Equatable {
-        
+        case updateKanji(String)
+        case updateMeaning(String)
+        case updateOndoku(String)
+        case updateKundoku(String)
     }
     
     var body: some Reducer<State, Action> {
         Reduce { state, action in
             switch action {
+            case .updateKanji(let kanji):
+                state.kanji = String(kanji.suffix(1))
+                return .none
+            case .updateMeaning(let meaning):
+                state.meaning = meaning
+                return .none
+            case .updateOndoku(let ondoku):
+                state.ondoku = ondoku
+                return .none
+            case .updateKundoku(let kundoku):
+                state.kundoku = kundoku
+                return .none
             default: return .none
             }
         }
