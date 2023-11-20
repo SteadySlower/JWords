3c9c32a3fff55e4a465abc892f3e33578dd6fcab
jwmoon <moon910904@gmail.com>
2023-11-20 15:25:59 +0900

    feature: add KanjiInputView

--- JWords/Views/Kanji/InputKanji.swift
+++ JWords/Views/Kanji/InputKanji.swift
@@ -14,6 +14,18 @@ struct InputKanji: Reducer {
         var meaning: String
         var ondoku: String
         var kundoku: String
+        
+        init(
+            kanji: String = "",
+            meaning: String = "",
+            ondoku: String = "",
+            kundoku: String = ""
+        ) {
+            self.kanji = kanji
+            self.meaning = meaning
+            self.ondoku = ondoku
+            self.kundoku = kundoku
+        }
     }
     
     enum Action: Equatable {
@@ -27,7 +39,7 @@ struct InputKanji: Reducer {
         Reduce { state, action in
             switch action {
             case .updateKanji(let kanji):
-                state.kanji = String(kanji.suffix(1))
+                state.kanji = kanji
                 return .none
             case .updateMeaning(let meaning):
                 state.meaning = meaning
@@ -38,9 +50,59 @@ struct InputKanji: Reducer {
             case .updateKundoku(let kundoku):
                 state.kundoku = kundoku
                 return .none
-            default: return .none
             }
         }
     }
     
+}
+
+struct KanjiInputView: View {
+    
+    let store: StoreOf<InputKanji>
+    
+    var body: some View {
+        WithViewStore(store, observe: { $0 }) { vs in
+            VStack {
+                HStack {
+                    Text("한자")
+                    TextField("一", text: vs.binding(
+                        get: \.kanji,
+                        send: InputKanji.Action.updateKanji)
+                    )
+                }
+                HStack {
+                    Text("뜻")
+                    TextField("한 일", text: vs.binding(
+                        get: \.meaning,
+                        send: InputKanji.Action.updateMeaning)
+                    )
+                }
+                HStack {
+                    Text("음독")
+                    TextField(vs.kanji.isEmpty ? "いち、　いっ" : "",
+                      text: vs.binding(
+                        get: \.ondoku,
+                        send: InputKanji.Action.updateOndoku)
+                    )
+                }
+                HStack {
+                    Text("훈독")
+                    TextField(vs.kanji.isEmpty ? "ひと, ひとつ" : "",
+                      text: vs.binding(
+                        get: \.kundoku,
+                        send: InputKanji.Action.updateKundoku)
+                    )
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    KanjiInputView(
+        store: Store(
+            initialState: InputKanji.State(),
+            reducer: { InputKanji()._printChanges() }
+        )
+    )
 }
