6aee55da25617ce51668409cd4a73a3b4a7df22c
JW Moon <moon910904@gmail.com>
2024-01-06 14:35:39 +0900

    test: add SearchKanjiTest

--- JWordsUnitTests/SearchKanjiTest.swift
+++ JWordsUnitTests/SearchKanjiTest.swift
@@ -5,4 +5,33 @@
 //  Created by JW Moon on 1/6/24.
 //
 
-import Foundation
+import ComposableArchitecture
+import XCTest
+
+@testable import JWords
+
+@MainActor
+final class SearchKanjiTest: XCTestCase {
+    
+    func testUpdateQuery() async {
+        let searched: [Kanji] = .testMock(count: Random.int(from: 0, to: 100))
+        
+        let store = TestStore(
+            initialState: SearchKanji.State(),
+            reducer: { SearchKanji() },
+            withDependencies: {
+                $0.kanjiClient.search = { _ in searched }
+            }
+        )
+        
+        let query = Random.string
+
+        await store.send(.updateQuery(query)) {
+            $0.query = query
+        }
+        
+        await store.receive(.kanjiSearched(searched))
+    }
+    
+}
+
