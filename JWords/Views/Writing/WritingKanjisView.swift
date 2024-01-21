d46a8034ab9771b539d69f8ca4ec82168872f57d
JW Moon <moon910904@gmail.com>
2024-01-21 16:32:48 +0900

    chore: rebase main

---
+++ JWords/Views/Writing/WritingKanjisView.swift
@@ -0,0 +1,25 @@
+//
+//  WritingKanjisView.swift
+//  JWords
+//
+//  Created by JW Moon on 1/21/24.
+//
+
+import SwiftUI
+
+struct WritingKanjisView: View {
+    
+    let kanjis: [Kanji]
+    
+    var body: some View {
+        HStack {
+            WritingKanjiList(kanjis: kanjis)
+            WritingKanjiView(kanji: .init(index: 0))
+        }
+    }
+}
+
+
+#Preview {
+    WritingKanjisView(kanjis: .mock)
+}
