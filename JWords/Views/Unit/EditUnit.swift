865d604b2082615192e486d15f870fb66d730fc9
JW Moon <moon910904@gmail.com>
2024-03-01 19:19:08 +0900

    refactor: refactor reducer body of EditUnit

--- JWords/Views/Unit/EditUnit.swift
+++ JWords/Views/Unit/EditUnit.swift
@@ -64,15 +64,10 @@ struct EditUnit {
     var body: some Reducer<State, Action> {
         Reduce { state, action in
             switch action {
-            case .inputUnit(let action):
-                switch action {
-                case .alreadyExist(let unit):
-                    if let unit = unit,
-                       unit.kanjiText != state.unit.kanjiText {
-                        state.setUneditableAlert()
-                    }
-                    return .none
-                default: return .none
+            case .inputUnit(.alreadyExist(let unit)):
+                if let unit = unit,
+                   unit.kanjiText != state.unit.kanjiText {
+                    state.setUneditableAlert()
                 }
             case .edit:
                 let input = StudyUnitInput(
@@ -81,18 +76,14 @@ struct EditUnit {
                     meaningText: state.meaningText)
                 let unit = try! unitClient.edit(state.unit, input)
                 return .send(.edited(unit))
-            case .alert(let action):
-                if action == .presented(.cancel) {
-                    return .send(.cancel)
-                }
-                return .none
-            default: return .none
+            case .alert(.presented(.cancel)):
+                return .send(.cancel)
+            default: break
             }
+            return .none
         }
-        .ifLet(\.$alert, action: /Action.alert)
-        Scope(state: \.inputUnit, action: /Action.inputUnit) {
-            InputUnit()
-        }
+        .ifLet(\.$alert, action: \.alert)
+        Scope(state: \.inputUnit, action: \.inputUnit) { InputUnit() }
     }
     
 }
