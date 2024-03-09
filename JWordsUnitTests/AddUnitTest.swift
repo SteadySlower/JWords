517c333b61c824f3f77126fb3d6cee45ff7f7853
JW Moon <moon910904@gmail.com>
2024-03-09 21:38:05 +0900

    refactor: refactor AddUnitTest

--- JWordsUnitTests/AddUnitTest.swift
+++ JWordsUnitTests/AddUnitTest.swift
@@ -13,7 +13,7 @@ import XCTest
 @MainActor
 final class AddUnitTest: XCTestCase {
     
-    func testInputUnitAlreadyExist() async {
+    func test_inputUnit_alreadyExist() async {
         let store = TestStore(
             initialState: AddUnit.State(),
             reducer: { AddUnit() }
@@ -28,7 +28,7 @@ final class AddUnitTest: XCTestCase {
         }
     }
     
-    func testAddNoSet() async {
+    func test_add_no_set() async {
         let store = TestStore(
             initialState: AddUnit.State(),
             reducer: { AddUnit() }
@@ -39,14 +39,11 @@ final class AddUnitTest: XCTestCase {
         }
     }
     
-    func testAddAlreadyExist() async {
+    func test_add_alreadyExist() async {
         let inserted: StudyUnit = .testMock
         
         let store = TestStore(
-            initialState: AddUnit.State(
-                set: .testMock,
-                alreadyExist: .testMock
-            )
+            initialState: AddUnit.State(set: .testMock, alreadyExist: .testMock)
         ) {
             AddUnit()
         } withDependencies: {
@@ -55,17 +52,14 @@ final class AddUnitTest: XCTestCase {
         }
 
         await store.send(.add)
-        
         await store.receive(.added(inserted))
     }
     
-    func testAdd() async {
+    func test_add_not_alreadyExist() async {
         let inserted: StudyUnit = .testMock
         
         let store = TestStore(
-            initialState: AddUnit.State(
-                set: .testMock
-            )
+            initialState: AddUnit.State(set: .testMock)
         ) {
             AddUnit()
         } withDependencies: {
@@ -73,8 +67,21 @@ final class AddUnitTest: XCTestCase {
         }
 
         await store.send(.add)
-        
         await store.receive(.added(inserted))
+    }
+    
+    func test_cancel() async {
+        let isDismissInvoked: LockIsolated<[Bool]> = .init([])
+        let store = TestStore(
+            initialState: AddUnit.State(),
+            reducer: { AddUnit() },
+            withDependencies: {
+                $0.dismiss = DismissEffect { isDismissInvoked.withValue { $0.append(true) } }
+            }
+        )
+        
+        await store.send(.cancel)
+        XCTAssertEqual(isDismissInvoked.value, [true])
     }
     
 }
