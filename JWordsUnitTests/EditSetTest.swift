d9a556acc220e42c545e97b2e8f3bec78954413b
JW Moon <moon910904@gmail.com>
2024-03-10 15:14:54 +0900

    refactor: refactor EditSetTest

--- JWordsUnitTests/EditSetTest.swift
+++ JWordsUnitTests/EditSetTest.swift
@@ -10,9 +10,10 @@ import XCTest
 
 @testable import JWords
 
-@MainActor
+
 final class EditSetTest: XCTestCase {
     
+    @MainActor
     func test_edit() async {
         let set: StudySet = .testMock
         let store = TestStore(
@@ -25,6 +26,21 @@ final class EditSetTest: XCTestCase {
         await store.send(.edit)
         
         await store.receive(.edited(set))
+    }
+    
+    @MainActor
+    func test_cancel() async {
+        let isDismissInvoked: LockIsolated<[Bool]> = .init([])
+        let store = TestStore(
+            initialState: EditSet.State(.testMock),
+            reducer: { EditSet() },
+            withDependencies: {
+                $0.dismiss = DismissEffect { isDismissInvoked.withValue { $0.append(true) } }
+            }
+        )
+        
+        await store.send(.cancel)
+        XCTAssertEqual(isDismissInvoked.value, [true])
     }
     
 }
