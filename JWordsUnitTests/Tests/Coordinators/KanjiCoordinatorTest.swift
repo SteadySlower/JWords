a71859348d4e9338eeb18976027cb3a972c55d50
JW Moon <moon910904@gmail.com>
2024-03-17 23:42:14 +0900

    test: add KanjiCoordinatorTest

--- JWordsUnitTests/Tests/Coordinators/KanjiCoordinatorTest.swift
+++ JWordsUnitTests/Tests/Coordinators/KanjiCoordinatorTest.swift
@@ -10,5 +10,22 @@ import XCTest
 @testable import JWords
 
 final class KanjiCoordinatorTest: XCTestCase {
-    #warning("TODO")
+    @MainActor
+    func test_kanjiList_showSamples() async {
+        let kanjis: [Kanji] = .testMock(count: Random.int(from: 1, to: 100))
+        let units: [StudyUnit] = .testMock
+        let store = TestStore(
+            initialState: KanjiCoordinator.State(
+                kanjiList: .init(kanjis: IdentifiedArray(uniqueElements: kanjis.map { DisplayKanji.State(kanji: $0) }))
+            ),
+            reducer: { KanjiCoordinator() },
+            withDependencies: {
+                $0.kanjiClient.kanjiUnits = { _ in units }
+            }
+        )
+        let kanji = kanjis.randomElement()!
+        await store.send(.kanjiList(.kanji(.element(id: kanji.id, action: .showSamples(kanji))))) {
+            $0.path.append(.samples(StudyKanjiSamples.State(kanji: kanji, units: units)))
+        }
+    }
 }
