fb15256ef070c9d9398ffb4daff99994b5ee8627
JW Moon <moon910904@gmail.com>
2024-03-17 23:33:10 +0900

    test: add SetCoordinatorTest

--- JWordsUnitTests/Tests/Coordinators/SetCoordinatorTest.swift
+++ JWordsUnitTests/Tests/Coordinators/SetCoordinatorTest.swift
@@ -10,5 +10,32 @@ import XCTest
 @testable import JWords
 
 final class SetCoordinatorTest: XCTestCase {
-    #warning("TODO")
+    @MainActor
+    func test_homeList_toStudySet() async {
+        let units: [StudyUnit] = .testMock
+        let store = TestStore(
+            initialState: SetCoordinator.State(),
+            reducer: { SetCoordinator() },
+            withDependencies: {
+                $0.studyUnitClient.fetch = { _ in units }
+            }
+        )
+        let set: StudySet = .testMock
+        await store.send(.homeList(.toStudySet(set))) {
+            $0.path.append(.studyUnitsInSet(StudyUnitsInSet.State(set: set, units: units)))
+        }
+    }
+    
+    @MainActor
+    func test_path_element_studyUnitsInSet_unitsMoved() async {
+        let store = TestStore(
+            initialState: SetCoordinator.State(path: StackState([
+                .studyUnitsInSet(.init(set: .testMock, units: .testMock))
+            ])),
+            reducer: { SetCoordinator() }
+        )
+        await store.send(.path(.element(id: 0, action: .studyUnitsInSet(.modals(.unitsMoved))))) {
+            $0.path.pop(from: 0)
+        }
+    }
 }
