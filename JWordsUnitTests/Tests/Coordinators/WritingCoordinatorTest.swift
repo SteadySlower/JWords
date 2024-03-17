5bed5b9e4a7649927d943f21c49a76ad74cb1f7f
JW Moon <moon910904@gmail.com>
2024-03-17 23:45:25 +0900

    test: add WritingCoordinatorTest

--- JWordsUnitTests/Tests/Coordinators/WritingCoordinatorTest.swift
+++ JWordsUnitTests/Tests/Coordinators/WritingCoordinatorTest.swift
@@ -10,5 +10,18 @@ import XCTest
 @testable import JWords
 
 final class WritingCoordinatorTest: XCTestCase {
-    #warning("TODO")
+    @MainActor
+    func test_kanjiSetList_setSelected() async {
+        let kanjis: [Kanji] = .testMock(count: Random.int(from: 0, to: 100))
+        let store = TestStore(
+            initialState: WritingCoordinator.State(),
+            reducer: { WritingCoordinator() },
+            withDependencies: {
+                $0.writingKanjiClient.fetch = { _ in kanjis }
+            }
+        )
+        await store.send(.kanjiSetList(.setSelected(.testMock))) {
+            $0.path.append(.writing(.init(kanjis: kanjis)))
+        }
+    }
 }
