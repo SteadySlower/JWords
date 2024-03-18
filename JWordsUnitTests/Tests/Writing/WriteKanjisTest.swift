8290947919a285f68d0210f6a25230436b321f44
JW Moon <moon910904@gmail.com>
2024-03-18 23:21:26 +0900

    test: add WriteKanjisTest

--- JWordsUnitTests/Tests/Writing/WriteKanjisTest.swift
+++ JWordsUnitTests/Tests/Writing/WriteKanjisTest.swift
@@ -10,5 +10,18 @@ import XCTest
 @testable import JWords
 
 final class WriteKanjisTest: XCTestCase {
-    #warning("TODO")
+
+    @MainActor
+    func test_kanjis_kanjiSelected() async {
+        let kanjis: [Kanji] = .testMock(count: Random.int(from: 1, to: 100))
+        let store = TestStore(
+            initialState: WriteKanjis.State(kanjis: kanjis),
+            reducer: { WriteKanjis() }
+        )
+        let kanji = kanjis.randomElement()!
+        await store.send(.kanjis(.kanjiSelected(kanji))) {
+            $0.toWrite.setKanji(kanji)
+        }
+    }
+    
 }
