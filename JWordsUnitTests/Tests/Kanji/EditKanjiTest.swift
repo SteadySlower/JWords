82e113c2622056b33d315a5442e34532a8075e68
JW Moon <moon910904@gmail.com>
2024-03-20 23:23:37 +0900

    test: add EditKanjiTest

--- JWordsUnitTests/Tests/Kanji/EditKanjiTest.swift
+++ JWordsUnitTests/Tests/Kanji/EditKanjiTest.swift
@@ -10,5 +10,32 @@ import XCTest
 @testable import JWords
 
 final class EditKanjiTest: XCTestCase {
-    #warning("TODO")
+    
+    @MainActor
+    func test_edit() async {
+        let kanji: Kanji = .testMock
+        let store = TestStore(
+            initialState: EditKanji.State(.testMock),
+            reducer: { EditKanji() },
+            withDependencies: {
+                $0.kanjiClient.edit = { _, _ in kanji }
+            }
+        )
+        await store.send(.edit)
+        await store.receive(.edited(kanji))
+    }
+    
+    @MainActor
+    func test_cancel() async {
+        let isDismissInvoked: LockIsolated<[Bool]> = .init([])
+        let store = TestStore(
+            initialState: EditKanji.State(.testMock),
+            reducer: { EditKanji() },
+            withDependencies: {
+                $0.dismiss = DismissEffect { isDismissInvoked.withValue { $0.append(true) } }
+            }
+        )
+        await store.send(.cancel)
+        XCTAssertEqual(isDismissInvoked.value, [true])
+    }
 }
