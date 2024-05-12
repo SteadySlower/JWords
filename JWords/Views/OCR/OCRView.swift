240ef9fe39a97be7c1aa6bac65959550829f5a97
JW Moon <moon910904@gmail.com>
2024-05-12 23:59:25 +0900

    feature: add isAuto

--- JWords/Views/OCR/OCRView.swift
+++ JWords/Views/OCR/OCRView.swift
@@ -9,6 +9,7 @@ import ComposableArchitecture
 import SwiftUI
 import OCRKit
 import OCRClient
+import UtilClient
 
 @Reducer
 struct OCR {
@@ -17,6 +18,11 @@ struct OCR {
         var getImage = GetImageForOCR.State()
         var ocr: GetTextsFromOCR.State?
         var ocrWithCrop: OCRwithCroppedImage.State?
+        var isAuto: Bool = true
+        
+        var isOCRAvailable: Bool {
+            ocr != nil && ocrWithCrop != nil
+        }
     }
     
     enum Action: Equatable {
@@ -33,12 +39,15 @@ struct OCR {
     }
     
     @Dependency(OCRClient.self) var ocrClient
+    @Dependency(UtilClient.self) var utilClient
     
     var body: some Reducer<State, Action> {
         Reduce { state, action in
             switch action {
             case .getImage(.imageFetched(let image)):
-                state.ocr = GetTextsFromOCR.State(image: image)
+                state.ocrWithCrop = .init(image: image)
+                let resized = utilClient.resizeImage(image, Constants.Size.deviceWidth)
+                state.ocr = GetTextsFromOCR.State(image: resized!)
                 return .merge(
                     .run { send in
                         await send(.koreanOcrResponse(TaskResult { try await ocrClient.ocr(image, .korean)}))
@@ -61,9 +70,9 @@ struct OCR {
                 state.ocr = nil
                 state.ocrWithCrop = nil
             case .changeToAuto:
-                state.ocrWithCrop = nil
+                state.isAuto = true
             case .changeToManual:
-                state.ocrWithCrop = .init(image: state.ocr!.image)
+                state.isAuto = false
             default: break
             }
             return .none
@@ -80,13 +89,13 @@ struct OCRView: View {
     let store: StoreOf<OCR>
     
     var body: some View {
-        if let ocrCropStore = store.scope(state: \.ocrWithCrop, action: \.ocrWithCrop) {
+        if let ocrCropStore = store.scope(state: \.ocrWithCrop, action: \.ocrWithCrop), !store.isAuto {
             VStack {
                 CropOCRView(store: ocrCropStore)
                 autoScanButton { store.send(.changeToAuto) }
                 RemoveImageButton { store.send(.removeImage) }
             }
-        } else if let ocrResultStore = store.scope(state: \.ocr, action: \.ocr) {
+        } else if let ocrResultStore = store.scope(state: \.ocr, action: \.ocr), store.isAuto {
             VStack {
                 OCRResultView(store: ocrResultStore)
                 if UIDevice.current.userInterfaceIdiom == .pad {
